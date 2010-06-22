f91c990f8f57f3441991c2c7f569f101b94ecdb9
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2010-06-22 18:38:40 +0000

    insta-arena fix
    Former-commit-id: 2c1353da52765ce5f01d5431b69fe96cff214f41
    Former-commit-id: 51de687e782299200f5fc95f488e42070a102d03

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -322,16 +322,8 @@ namespace game
                 break;
             }
             if(d->loadweap < WEAP_OFFSET || d->loadweap >= WEAP_ITEM) d->loadweap = WEAP_MELEE;
-            if(WEAP(d->loadweap, allowed) >= (m_limited(gamemode, mutators) ? 2 : 1))
-            {
-                client::addmsg(N_LOADWEAP, "ri2", d->clientnum, d->loadweap);
-                conoutft(CON_SELF, "you will spawn with: %s%s", weaptype[d->loadweap].text, (d->loadweap >= WEAP_OFFSET ? weaptype[d->loadweap].name : "random weapons"));
-            }
-            else
-            {
-                conoutft(CON_SELF, "sorry, the \fs%s%s\fS has been disabled, please select a different weapon", weaptype[d->loadweap].text, (d->loadweap >= WEAP_OFFSET ? weaptype[d->loadweap].name : "random weapons"));
-                d->loadweap = -1;
-            }
+            client::addmsg(N_LOADWEAP, "ri2", d->clientnum, d->loadweap);
+            conoutft(CON_SELF, "setting weapon to: %s%s", weaptype[d->loadweap].text, (d->loadweap >= WEAP_OFFSET ? weaptype[d->loadweap].name : "random weapons"));
         }
         else conoutft(CON_MESG, "\foweapon selection is only available in arena");
     }
