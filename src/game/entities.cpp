31e679fb738f4299d40d2f216892e22fe9e608d3
Quinton Reeves <qreeves@gmail.com>
2010-07-11 22:45:35 +0000

    swap weighting multipliers
    Former-commit-id: 3c5394edf600bea67be799d93d65a95ffad23e6a
    Former-commit-id: 44acbddae814aee18fa08e2ceeac516972749db2

--- src/game/entities.cpp
+++ src/game/entities.cpp
@@ -35,14 +35,14 @@ namespace entities
         if(clipped(pos, true) || !insideworld(vec(pos.x, pos.y, min(pos.z, getworldsize() - 1e-3f)))) return -1;
         float dist = raycube(pos, vec(0, 0, -1), 0, RAY_CLIPMAT);
         int posmat = lookupmaterial(pos), weight = 1;
-        if(isliquid(posmat&MATF_VOLUME)) weight *= 2;
+        if(isliquid(posmat&MATF_VOLUME)) weight *= 5;
         if(dist >= 0)
         {
             weight = int(dist/ai::JUMPMIN);
             pos.z -= min(dist, pos.z);
             int trgmat = lookupmaterial(pos);
             if(trgmat&MAT_DEATH || (trgmat&MATF_VOLUME) == MAT_LAVA) weight *= 10;
-            else if(isliquid(trgmat&MATF_VOLUME)) weight *= 5;
+            else if(isliquid(trgmat&MATF_VOLUME)) weight *= 2;
         }
         return weight;
     }
