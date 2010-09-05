8edb9d9709ed2a2fadc39ea00392c2b988a9be70
Quinton Reeves <qreeves@gmail.com>
2010-09-05 20:23:29 +0000

    bloodsparks var, explosion sparks, shotgun altfire tweak
    Former-commit-id: 276de5812f19fee530bc833c2eefec83dd3cee92
    Former-commit-id: c280065f9573002275f2449e06264f1d444c2fcb

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -106,6 +106,7 @@ namespace game
     FVAR(IDF_PERSIST, bloodscale, 0, 1, 1000);
     VAR(IDF_PERSIST, bloodfade, 1, 5000, INT_MAX-1);
     VAR(IDF_PERSIST, bloodsize, 1, 20, 1000);
+    VAR(IDF_PERSIST, bloodsparks, 0, 0, 1);
     FVAR(IDF_PERSIST, debrisscale, 0, 1, 1000);
     VAR(IDF_PERSIST, debrisfade, 1, 5000, INT_MAX-1);
     FVAR(IDF_PERSIST, gibscale, 0, 1, 1000);
@@ -671,7 +672,8 @@ namespace game
                     {
                         if(!kidmode && bloodscale > 0)
                             part_splash(PART_BLOOD, int(clamp(damage/2, 2, 10)*bloodscale), bloodfade, p, 0x88FFFF, (rnd(bloodsize)+1)/10.f, 1, 200, DECAL_BLOOD, int(d->radius*8));
-                        else part_splash(PART_HINT, int(clamp(damage/2, 2, 10)), bloodfade, p, 0xFFFF88, 1.5f, 1, 50, DECAL_STAIN, int(d->radius*4));
+                        if(kidmode || bloodscale <= 0 || bloodsparks)
+                            part_splash(PART_HINT, int(clamp(damage/2, 2, 10)), bloodfade, p, 0xFFFF88, 1.5f, 1, 50, DECAL_STAIN, int(d->radius*4));
                     }
                     if(d->aitype < AI_START && !issound(d->vschan)) playsound(S_PAIN+rnd(S_R_PAIN), d->o, d, 0, -1, -1, -1, &d->vschan);
                     if(!burning) d->quake = clamp(d->quake+max(damage/2, 1), 0, 1000);
