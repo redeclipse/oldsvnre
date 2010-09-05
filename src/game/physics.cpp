e1928118f9c344c27fbc43af32685db0925195c5
Quinton Reeves <qreeves@gmail.com>
2010-09-05 21:42:00 +0000

    decrease impulseturn var to only activate on wall running
    Former-commit-id: 06c73d6af0f7527d82d9f1e6063f112ab0c79d07
    Former-commit-id: 98e550ed5fdcc92f5f4067eba11afa8ea4f36438

--- src/game/physics.cpp
+++ src/game/physics.cpp
@@ -25,7 +25,7 @@ namespace physics
 
     VAR(IDF_PERSIST, impulseaction, 0, 1, 2); // determines if impulse remains active when pushed, 0 = off, 1 = only if no gravity or impulsestyle requires no ground contact, 2 = always
     FVAR(IDF_PERSIST, impulsekick, 0, 135, 179.9f); // determines the minimum angle to switch between wall kick and run
-    VAR(IDF_PERSIST, impulseturn, 0, 2, 2); // determines if parkour actions force turning, 0 = off, 1 = only wall run, 2 = wall run and kick
+    VAR(IDF_PERSIST, impulseturn, 0, 1, 2); // determines if parkour actions force turning, 0 = off, 1 = only wall run, 2 = wall run and kick
     VAR(IDF_PERSIST, dashaction, 0, 3, 3); // determines how dash action works, 0 = off, 1 = double jump, 2 = double tap, 3 = both
 
     VAR(IDF_PERSIST, crouchstyle, 0, 1, 2); // 0 = press and hold, 1 = double-tap toggle, 2 = toggle
