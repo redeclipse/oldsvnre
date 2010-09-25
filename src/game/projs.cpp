f43b00e6d244d862fc0b761ef61207f1498fe9a3
Quinton Reeves <qreeves@gmail.com>
2010-09-25 23:38:46 +0000

    fix weapon push values, radiating weapons now wavepush in their defined area as they travel (meaning plasma alt-fire can suck you into its killzone from a distance), tweaked some damage settings down a bit (esp. in regard to shotgun/smg/flamer alt-fires)
    Former-commit-id: 3f546b6c0694bbd0793e6570556adfa49ce74d21
    Former-commit-id: aee1ac49a76984dbf2638eb7e1cb7787bc1c6632

--- src/game/projs.cpp
+++ src/game/projs.cpp
@@ -34,7 +34,7 @@ namespace projs
     VAR(IDF_PERSIST, projtraildelay, 1, 25, INT_MAX-1);
     VAR(IDF_PERSIST, projtraillength, 1, 500, INT_MAX-1);
     VAR(IDF_PERSIST, projfirehint, 0, 1, 1);
-    VAR(IDF_PERSIST, projteamhint, 0, 1, 1);
+    VAR(IDF_PERSIST, projteamhint, 0, 0, 1);
     #define teamhint(a,b) (projteamhint ? teamtype[a].colour : b)
 
     VAR(IDF_PERSIST, muzzleflash, 0, 3, 3); // 0 = off, 1 = only other players, 2 = only thirdperson, 3 = all
@@ -170,8 +170,8 @@ namespace projs
 
     bool radialeffect(gameent *d, projent &proj, bool explode, int radius)
     {
-        bool radiated = false;
-        float maxdist = explode ? radius*WEAP(proj.weap, pusharea) : radius, dist = 1e16f;
+        bool radiated = false, push = WEAP(proj.weap, pusharea) > 1;
+        float maxdist = push ? radius*WEAP(proj.weap, pusharea) : radius, dist = 1e16f;
         if(d->type == ENT_PLAYER || d->type == ENT_AI)
         {
             int flags = 0;
@@ -207,7 +207,7 @@ namespace projs
                 hitpush(d, proj, flags|(explode ? HIT_EXPLODE : HIT_BURN), radius, dist, proj.scale);
                 radiated = true;
             }
-            else if(explode && dist <= maxdist)
+            else if(push && dist <= maxdist)
             {
                 hitpush(d, proj, flags|HIT_WAVE, radius, dist, proj.scale);
                 radiated = true;
@@ -1660,18 +1660,15 @@ namespace projs
                 if(!proj.child && weaptype[proj.weap].traced) proj.o = proj.to;
                 if(proj.state == CS_DEAD)
                 {
-                    if(radius)
+                    if(!(proj.projcollide&COLLIDE_CONT)) proj.hit = NULL;
+                    if(!proj.limited && radius > 0) loopj(game::numdynents(true))
                     {
-                        if(!(proj.projcollide&COLLIDE_CONT)) proj.hit = NULL;
-                        if(!proj.limited && radius > 0) loopj(game::numdynents(true))
-                        {
-                            gameent *f = (gameent *)game::iterdynents(j, true);
-                            if(!f || f->state != CS_ALIVE || !physics::issolid(f, &proj, false)) continue;
-                            radialeffect(f, proj, true, radius);
-                        }
+                        gameent *f = (gameent *)game::iterdynents(j, true);
+                        if(!f || f->state != CS_ALIVE || !physics::issolid(f, &proj, false)) continue;
+                        radialeffect(f, proj, true, radius);
                     }
                 }
-                else if(WEAP2(proj.weap, radial, proj.flags&HIT_ALT) && radius)
+                else if(WEAP2(proj.weap, radial, proj.flags&HIT_ALT))
                 {
                     if(!(proj.projcollide&COLLIDE_CONT)) proj.hit = NULL;
                     if(!proj.limited && radius > 0 && (!proj.lastradial || lastmillis-proj.lastradial >= 40)) loopj(game::numdynents())
