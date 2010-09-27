bec847594d05a5df5a056a2ebf347fd750d0d022
eihrul <eihrul@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2010-09-27 18:56:17 +0000

    model command cleanups
    Former-commit-id: d5a95daeafc24b5b3cc6b78e3cbedfd4504577a3
    Former-commit-id: 9d8657d026d0d9a05854692cde0d543b64496d39

--- src/engine/md3.h
+++ src/engine/md3.h
@@ -1,9 +1,5 @@
 struct md3;
 
-md3 *loadingmd3 = NULL;
-
-string md3dir;
-
 struct md3frame
 {
     vec bbmin, bbmax, origin;
@@ -48,10 +44,11 @@ struct md3meshheader
     int ofs_triangles, ofs_shaders, ofs_uv, ofs_vertices, meshsize; // offsets
 };
 
-struct md3 : vertmodel
+struct md3 : vertmodel, vertloader<md3>
 {
     md3(const char *name) : vertmodel(name) {}
 
+    static const char *formatname() { return "md3"; }
     int type() const { return MDL_MD3; }
 
     struct md3meshgroup : vertmeshgroup
@@ -203,18 +200,18 @@ struct md3 : vertmodel
     bool load()
     {
         if(loaded) return true;
-        formatstring(md3dir)("models/%s", loadname);
+        formatstring(dir)("models/%s", loadname);
         defformatstring(cfgname)("models/%s/md3.cfg", loadname);
 
-        loadingmd3 = this;
+        loading = this;
         if(execfile(cfgname, false) && parts.length()) // configured md3, will call the md3* commands below
         {
-            loadingmd3 = NULL;
+            loading = NULL;
             loopv(parts) if(!parts[i]->meshes) return false;
         }
         else // md3 without configuration, try default tris and skin
         {
-            loadingmd3 = NULL;
+            loading = NULL;
             if(!loaddefaultparts()) return false;
         }
         scale /= 4;
@@ -226,186 +223,5 @@ struct md3 : vertmodel
     }
 };
 
-void md3load(char *model)
-{
-    if(!loadingmd3) { conoutf("\frnot loading an md3"); return; }
-    defformatstring(filename)("%s/%s", md3dir, model);
-    md3::part &mdl = *new md3::part;
-    loadingmd3->parts.add(&mdl);
-    mdl.model = loadingmd3;
-    mdl.index = loadingmd3->parts.length()-1;
-    if(mdl.index) mdl.pitchscale = mdl.pitchoffset = mdl.pitchmin = mdl.pitchmax = 0;
-    mdl.meshes = loadingmd3->sharemeshes(path(filename));
-    if(!mdl.meshes) conoutf("\frcould not load %s", filename); // ignore failure
-    else mdl.initskins();
-}
-
-void md3pitch(float *pitchscale, float *pitchoffset, float *pitchmin, float *pitchmax)
-{
-    if(!loadingmd3 || loadingmd3->parts.empty()) { conoutf("\frnot loading an md3"); return; }
-    md3::part &mdl = *loadingmd3->parts.last();
-
-    mdl.pitchscale = *pitchscale;
-    mdl.pitchoffset = *pitchoffset;
-    if(*pitchmin || *pitchmax)
-    {
-        mdl.pitchmin = *pitchmin;
-        mdl.pitchmax = *pitchmax;
-    }
-    else
-    {
-        mdl.pitchmin = -360*mdl.pitchscale;
-        mdl.pitchmax = 360*mdl.pitchscale;
-    }
-}
-
-#define loopmd3meshes(meshname, m, body) \
-    if(!loadingmd3 || loadingmd3->parts.empty()) { conoutf("\frnot loading an md3"); return; } \
-    md3::part &mdl = *loadingmd3->parts.last(); \
-    if(!mdl.meshes) return; \
-    loopv(mdl.meshes->meshes) \
-    { \
-        md3::vertmesh &m = *(md3::vertmesh *)mdl.meshes->meshes[i]; \
-        if(!strcmp(meshname, "*") || !strcmp(m.name, meshname)) \
-        { \
-            body; \
-        } \
-    }
-
-#define loopmd3skins(meshname, s, body) loopmd3meshes(meshname, m, { md3::skin &s = mdl.skins[i]; body; })
-
-void md3skin(char *meshname, char *tex, char *masks, float *envmapmax, float *envmapmin)
-{
-    loopmd3skins(meshname, s,
-        s.tex = textureload(makerelpath(md3dir, tex), 0, true, false);
-        if(*masks)
-        {
-            s.masks = textureload(makerelpath(md3dir, masks, "<stub>"), 0, true, false);
-            s.envmapmax = *envmapmax;
-            s.envmapmin = *envmapmin;
-        }
-    );
-}
-
-void md3spec(char *meshname, int *percent)
-{
-    float spec = 1.0f;
-    if(*percent>0) spec = *percent/100.0f;
-    else if(*percent<0) spec = 0.0f;
-    loopmd3skins(meshname, s, s.spec = spec);
-}
-
-void md3ambient(char *meshname, int *percent)
-{
-    float ambient = 0.3f;
-    if(*percent>0) ambient = *percent/100.0f;
-    else if(*percent<0) ambient = 0.0f;
-    loopmd3skins(meshname, s, s.ambient = ambient);
-}
-
-void md3glow(char *meshname, int *percent)
-{
-    float glow = 3.0f;
-    if(*percent>0) glow = *percent/100.0f;
-    else if(*percent<0) glow = 0.0f;
-    loopmd3skins(meshname, s, s.glow = glow);
-}
-
-void md3glare(char *meshname, float *specglare, float *glowglare)
-{
-    loopmd3skins(meshname, s, { s.specglare = *specglare; s.glowglare = *glowglare; });
-}
-
-void md3alphatest(char *meshname, float *cutoff)
-{
-    loopmd3skins(meshname, s, s.alphatest = max(0.0f, min(1.0f, *cutoff)));
-}
-
-void md3alphablend(char *meshname, int *blend)
-{
-    loopmd3skins(meshname, s, s.alphablend = *blend!=0);
-}
-
-void md3cullface(char *meshname, int *cullface)
-{
-    loopmd3skins(meshname, s, s.cullface = *cullface!=0);
-}
-
-void md3material(char *meshname, int *material)
-{
-    loopmd3skins(meshname, s, s.material = *material!=0);
-}
-
-void md3envmap(char *meshname, char *envmap)
-{
-    Texture *tex = cubemapload(envmap);
-    loopmd3skins(meshname, s, s.envmap = tex);
-}
-
-void md3bumpmap(char *meshname, char *normalmap, char *skin)
-{
-    Texture *normalmaptex = NULL, *skintex = NULL;
-    normalmaptex = textureload(makerelpath(md3dir, normalmap, "<noff>"), 0, true, false);
-    if(skin[0]) skintex = textureload(makerelpath(md3dir, skin, "<noff>"), 0, true, false);
-    loopmd3skins(meshname, s, { s.unlittex = skintex; s.normalmap = normalmaptex; m.calctangents(); });
-}
-
-void md3fullbright(char *meshname, float *fullbright)
-{
-    loopmd3skins(meshname, s, s.fullbright = *fullbright);
-}
-
-void md3shader(char *meshname, char *shader)
-{
-    loopmd3skins(meshname, s, s.shader = lookupshaderbyname(shader));
-}
-
-void md3scroll(char *meshname, float *scrollu, float *scrollv)
-{
-    loopmd3skins(meshname, s, { s.scrollu = *scrollu; s.scrollv = *scrollv; });
-}
-
-void md3anim(char *anim, int *frame, int *range, float *speed, int *priority)
-{
-    if(!loadingmd3 || loadingmd3->parts.empty()) { conoutf("\frnot loading an md3"); return; }
-    vector<int> anims;
-    game::findanims(anim, anims);
-    if(anims.empty()) conoutf("\frcould not find animation %s in %s", anim, loadingmd3->loadname);
-    else loopv(anims)
-    {
-        loadingmd3->parts.last()->setanim(0, anims[i], *frame, *range, *speed, *priority);
-    }
-}
-
-void md3link(int *parent, int *child, char *tagname, float *x, float *y, float *z)
-{
-    if(!loadingmd3) { conoutf("\frnot loading an md3"); return; }
-    if(!loadingmd3->parts.inrange(*parent) || !loadingmd3->parts.inrange(*child)) { conoutf("\frno models loaded to link"); return; }
-    if(!loadingmd3->parts[*parent]->link(loadingmd3->parts[*child], tagname, vec(*x, *y, *z))) conoutf("\frcould not link model %s", loadingmd3->loadname);
-}
-
-void md3noclip(char *meshname, int *noclip)
-{
-    loopmd3meshes(meshname, m, m.noclip = *noclip!=0);
-}
-
-COMMAND(0, md3load, "s");
-COMMAND(0, md3pitch, "ffff");
-COMMAND(0, md3skin, "sssff");
-COMMAND(0, md3spec, "si");
-COMMAND(0, md3ambient, "si");
-COMMAND(0, md3glow, "si");
-COMMAND(0, md3glare, "sff");
-COMMAND(0, md3alphatest, "sf");
-COMMAND(0, md3alphablend, "si");
-COMMAND(0, md3cullface, "si");
-COMMAND(0, md3material, "si");
-COMMAND(0, md3envmap, "ss");
-COMMAND(0, md3bumpmap, "sss");
-COMMAND(0, md3fullbright, "sf");
-COMMAND(0, md3shader, "ss");
-COMMAND(0, md3scroll, "sff");
-COMMAND(0, md3anim, "siifi");
-COMMAND(0, md3link, "iisfff");
-COMMAND(0, md3noclip, "si");
+vertcommands<md3> md3commands;
 
