42aa8b3eabb7b6f5be80788d7a651f320235a14e
Lee Salzman <lsalzman@gmail.com>
2010-10-04 16:41:07 +0000

    end of line comment fix
    Former-commit-id: cd6e9cdabb900fbf4cb4bdf5c501618f757d3a18
    Former-commit-id: 65caaf1ba1beccc4faf2e031da905f9b7dd6e552

--- src/engine/command.cpp
+++ src/engine/command.cpp
@@ -911,7 +911,7 @@ static bool compileblock(vector<uint> &code, const char *&p, int wordtype)
         switch(c)
         {
             case '\0':
-                debugcode(debugline(line, "\frmissing ]"));
+                debugcode(debugline(line, "\frmissing \"]\""));
                 p--;
                 return false;
             case '\"':
@@ -1152,18 +1152,23 @@ static void compilestatements(vector<uint> &code, const char *&p, int rettype, i
             delete[] idname;
         }
     endstatement:
-        switch(brak)
-        {
-            case ')': p += strcspn(p, ");\n\0"); break;
-            case ']': p += strcspn(p, "];\n\0"); break;
-            case '\0': p += strcspn(p, ";\n\0"); break;
-        }
+        p += strcspn(p, ")];/\n\0");
         int c = *p++;
-        if(c==brak) break;
-        else if(c=='\0')
+        switch(c)
         {
-            debugcode(debugline(line, "\frmissing \"%c\""), brak);
-            break;
+            case '\0':
+                if(c != brak) debugcode(debugline(line, "\frmissing \"%c\""), brak);
+                return;
+
+            case ')':
+            case ']':
+                if(c == brak) return;
+                debugcode(debugline(line, "\frunexpected \"%c\""), c);
+                break;
+
+            case '/':
+                if(*p == '/') p += strcspn(p, "\n\0");
+                goto endstatement;
         }
     }
 }
