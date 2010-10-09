059c6abee214cc2109bfb2f4ac0f29f68eedd18b
Quinton Reeves <qreeves@gmail.com>
2010-10-09 11:28:55 +0000

    smoother height adjustment when resizing that doesn't rely on physics to correct it
    Former-commit-id: f8bf67843d99a1a8be6f9340fda9cf3e494c6392
    Former-commit-id: a5ea35ec80c578ccaba0742f21f5441282833a1e

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -450,51 +450,6 @@ namespace game
     void setmode(int nmode, int nmuts) { server::modecheck(nextmode = nmode, nextmuts = nmuts); }
     ICOMMAND(0, mode, "ii", (int *val, int *mut), setmode(*val, *mut));
 
-    void heightoffset(gameent *d, bool local)
-    {
-        d->o.z -= d->height;
-        if(d->state == CS_ALIVE)
-        {
-            if(physics::iscrouching(d))
-            {
-                bool crouching = d->action[AC_CROUCH];
-                float crouchoff = 1.f-CROUCHHEIGHT;
-                if(!crouching)
-                {
-                    float z = d->o.z, zoff = d->zradius*crouchoff, zrad = d->zradius-zoff, frac = zoff/10.f;
-                    d->o.z += zrad;
-                    loopi(10)
-                    {
-                        d->o.z += frac;
-                        if(!collide(d, vec(0, 0, 1), 0.f, false))
-                        {
-                            crouching = true;
-                            break;
-                        }
-                    }
-                    if(crouching)
-                    {
-                        if(d->actiontime[AC_CROUCH] >= 0) d->actiontime[AC_CROUCH] = max(PHYSMILLIS-(lastmillis-d->actiontime[AC_CROUCH]), 0)-lastmillis;
-                    }
-                    else if(d->actiontime[AC_CROUCH] < 0)
-                        d->actiontime[AC_CROUCH] = lastmillis-max(PHYSMILLIS-(lastmillis+d->actiontime[AC_CROUCH]), 0);
-                    d->o.z = z;
-                }
-                if(d->type == ENT_PLAYER || d->type == ENT_AI)
-                {
-                    int crouchtime = abs(d->actiontime[AC_CROUCH]);
-                    float amt = lastmillis-crouchtime <= PHYSMILLIS ? clamp(float(lastmillis-crouchtime)/PHYSMILLIS, 0.f, 1.f) : 1.f;
-                    if(!crouching) amt = 1.f-amt;
-                    crouchoff *= amt;
-                }
-                d->height = d->zradius-(d->zradius*crouchoff);
-            }
-            else d->height = d->zradius;
-        }
-        else d->height = d->zradius;
-        d->o.z += d->height;
-    }
-
     float rescale(gameent *d)
     {
         float total = actorscale;
@@ -542,12 +497,57 @@ namespace game
         return total;
     }
 
+    void heightoffset(gameent *d)
+    {
+        d->o.z -= d->height;
+        d->setscale(rescale(d));
+        if(d->aitype < AI_START && d->state == CS_ALIVE)
+        {
+            if(physics::iscrouching(d))
+            {
+                bool crouching = d->action[AC_CROUCH];
+                float crouchoff = 1.f-CROUCHHEIGHT;
+                if(!crouching)
+                {
+                    float z = d->o.z, zoff = d->zradius*crouchoff, zrad = d->zradius-zoff, frac = zoff/10.f;
+                    d->o.z += zrad;
+                    loopi(10)
+                    {
+                        d->o.z += frac;
+                        if(!collide(d, vec(0, 0, 1), 0.f, false))
+                        {
+                            crouching = true;
+                            break;
+                        }
+                    }
+                    if(crouching)
+                    {
+                        if(d->actiontime[AC_CROUCH] >= 0) d->actiontime[AC_CROUCH] = max(PHYSMILLIS-(lastmillis-d->actiontime[AC_CROUCH]), 0)-lastmillis;
+                    }
+                    else if(d->actiontime[AC_CROUCH] < 0)
+                        d->actiontime[AC_CROUCH] = lastmillis-max(PHYSMILLIS-(lastmillis+d->actiontime[AC_CROUCH]), 0);
+                    d->o.z = z;
+                }
+                if(d->type == ENT_PLAYER || d->type == ENT_AI)
+                {
+                    int crouchtime = abs(d->actiontime[AC_CROUCH]);
+                    float amt = lastmillis-crouchtime <= PHYSMILLIS ? clamp(float(lastmillis-crouchtime)/PHYSMILLIS, 0.f, 1.f) : 1.f;
+                    if(!crouching) amt = 1.f-amt;
+                    crouchoff *= amt;
+                }
+                d->height = d->zradius-(d->zradius*crouchoff);
+            }
+            else d->height = d->zradius;
+        }
+        else d->height = d->zradius;
+        d->o.z += d->height;
+    }
+
     void checkoften(gameent *d, bool local)
     {
-        d->setscale(rescale(d));
+        heightoffset(d);
         d->checktags();
         adjustscaled(int, d->quake, quakefade);
-        if(d->aitype < AI_START) heightoffset(d, local);
         loopi(WEAP_MAX) if(d->weapstate[i] != WEAP_S_IDLE)
         {
             bool timeexpired = lastmillis-d->weaplast[i] >= d->weapwait[i]+(d->weapselect != i || d->weapstate[i] != WEAP_S_POWER ? 0 : PHYSMILLIS);
