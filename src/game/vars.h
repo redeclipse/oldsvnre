59a7f1552300287a32f8760744ba1858301b5f6e
Quinton Reeves <qreeves@gmail.com>
2011-03-26 10:52:02 +0000

    rename mode/mutslimit to mode/mutslockfilter to avoid killing the v1.0 menus
    Former-commit-id: e833b9d37c7fc717bc1191389748a241ee2eb71d
    Former-commit-id: 415092475d85b61d1164e83ce485d3453527a44f

--- src/game/vars.h
+++ src/game/vars.h
@@ -5,9 +5,9 @@ GSVAR(IDF_ADMIN, serverdesc, "");
 GSVAR(IDF_ADMIN, servermotd, "");
 GVAR(IDF_ADMIN, automaster, 0, 0, 1);
 
-GVAR(IDF_ADMIN, modelimit, 0, G_LIMIT, G_ALL);
-GVAR(IDF_ADMIN, mutslimit, 0, G_M_ALL, G_M_ALL);
 GVAR(IDF_ADMIN, modelock, 0, 3, 5); // 0 = off, 1 = master only (+1 admin only), 3 = master can only set limited mode and higher (+1 admin), 5 = no mode selection
+GVAR(IDF_ADMIN, modelockfilter, 0, G_LIMIT, G_ALL);
+GVAR(IDF_ADMIN, mutslockfilter, 0, G_M_ALL, G_M_ALL);
 GVAR(IDF_ADMIN, mapslock, 0, 3, 5); // 0 = off, 1 = master can select non-allow maps (+1 admin), 3 = master can select non-rotation maps (+1 admin), 5 = no map selection
 GVAR(IDF_ADMIN, varslock, 0, 1, 3); // 0 = off, 1 = master, 2 = admin only, 3 = nobody
 GVAR(IDF_ADMIN, votelock, 0, 2, 5); // 0 = off, 1 = master can select same game (+1 admin), 3 = master only can vote (+1 admin), 5 = no voting
