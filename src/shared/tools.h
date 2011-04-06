a9aee1e67b238ac6ccd35393da1e0952e9efff86
eihrul <eihrul@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2011-04-06 06:57:19 +0000

    redef fix
    Former-commit-id: db5ace4969d38ce852403f4082fe01c8f1902e3f
    Former-commit-id: 83d851611585d395e6aa6f449ba17ae86739d8f7

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -48,26 +48,6 @@ inline void *operator new[](size_t, void *p) { return p; }
 inline void operator delete(void *, void *) {}
 inline void operator delete[](void *, void *) {}
 
-inline void *operator new(size_t size) 
-{ 
-    void *p = malloc(size);
-    if(!p) abort();
-    return p;
-}
-inline void *operator new[](size_t size) 
-{
-    void *p = malloc(size);
-    if(!p) abort();
-    return p;
-}
-inline void operator delete(void *p) { if(p) free(p); }
-inline void operator delete[](void *p) { if(p) free(p); } 
-
-inline void *operator new(size_t, void *p) { return p; }
-inline void *operator new[](size_t, void *p) { return p; }
-inline void operator delete(void *, void *) {}
-inline void operator delete[](void *, void *) {}
-
 #ifdef swap
 #undef swap
 #endif
