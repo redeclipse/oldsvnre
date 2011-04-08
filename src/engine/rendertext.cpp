c93e96f068765db37c1ee3df15ca4aea9af1e3dc
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2011-04-08 13:18:48 +0000

    compile fix
    Former-commit-id: d7b938c13d3d7e6c7edd12539b8119cd4e68b75f
    Former-commit-id: 03b203778e4508ff5c6ac7e18ca8d891abfd0bec

--- src/engine/rendertext.cpp
+++ src/engine/rendertext.cpp
@@ -197,7 +197,7 @@ static int draw_icon(const char *name, int x, int y)
         { \
             if(end > start) \
             { \
-                string value; copystring(value, start, min(end - start + 1, MAXSTRLEN)); \
+                string value; copystring(value, start, min(int(end - start + 1), MAXSTRLEN)); \
                 TEXTICON(value); \
             } \
             h += end-start; \
