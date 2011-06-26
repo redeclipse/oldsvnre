8751949b3d9fa2112f61543ec7aad2be5ce27399
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2011-06-26 04:28:54 +0000

    rescale pitch when wall kicking so it is always zero or more (no point scaling walls downward)
    Former-commit-id: a493102319debd1a6f84790faebeae8dc9cb36f4
    Former-commit-id: 11eee282be8dfcf8abd12a9fb652a3c8f7cada29

--- src/game/physics.cpp
+++ src/game/physics.cpp
@@ -984,7 +984,7 @@ namespace physics
                                 float mag = impulsevelocity(d, impulseparkourkick);
                                 if(mag > 0)
                                 {
-                                    vecfromyawpitch(d->aimyaw, d->aimpitch, 1, 0, dir);
+                                    vecfromyawpitch(d->aimyaw, (90+d->aimpitch)*0.5f, 1, 0, dir);
                                     (d->vel = dir.normalize()).reflect(wall).normalize().mul(mag);
                                     d->doimpulse(impulsemeter ? impulsecost : 0, IM_T_KICK, lastmillis);
                                     d->turnmillis = PHYSMILLIS;
