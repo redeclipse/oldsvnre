e5ab2782f481529215dc1895ad1cd2e2e253651e
Lee Salzman <lsalzman@gmail.com>
2011-11-22 10:47:58 +0000

    add bolt hints instead of overlaying hint/edit particles
    Former-commit-id: c76ceb057adc8d00c0bb230e646da90aa7a597b6
    Former-commit-id: daab138a61f3b498778aef5265087efc44479625

--- src/game/entities.cpp
+++ src/game/entities.cpp
@@ -2540,8 +2540,7 @@ namespace entities
         if(enttype[e.type].usetype == EU_ITEM && (active || isedit))
         {
             float radius = max(((e.type == WEAPON ? weaptype[attr].halo : enttype[e.type].radius*0.5f)+(fluc*0.5f))*skew, 0.125f);
-            part_create(PART_HINT_SOFT, 1, o, colour, radius, fluc*skew);
-            part_create(PART_EDIT, 1, o, colour, radius*0.75f, fluc*skew);
+            part_create(PART_HINT_BOLD_SOFT, 1, o, colour, radius, fluc*skew);
         }
         if(isedit ? (showentinfo >= (hasent ? 2 : 3)) : (enttype[e.type].usetype == EU_ITEM && active && showentdescs >= 3))
         {
