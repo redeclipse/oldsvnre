c7fe056cfe4a01f0d80de549f43f5361c2343dff
Quinton Reeves <qreeves@gmail.com>
2011-12-22 08:24:22 +0000

    fixes
    Former-commit-id: f61502ba00ce46a9fd9c652a2c1bbad8fe1b2a04
    Former-commit-id: 9834912cd4dc9046458af58924add43e245da074

--- src/xcode/macutils.mm
+++ src/xcode/macutils.mm
@@ -1,38 +1,27 @@
 #import <Cocoa/Cocoa.h>
 #import <CoreFoundation/CFBundle.h>
 
-char *mac_pasteconsole(int *cblen)
-{
+#define MAXSTRLEN 512 // must be at least 512 bytes to comply with rfc1459
+typedef char string[MAXSTRLEN];
+inline char *s_strncpy(char *d, const char *s, size_t m) { strncpy(d, s, m); d[m-1] = 0; return d; };
+inline char *s_strcpy(char *d, const char *s, size_t m = MAXSTRLEN) { return s_strncpy(d, s, m); }
+inline char *s_strcat(char *d, const char *s) { size_t n = strlen(d); return s_strncpy(d+n, s, MAXSTRLEN-n); };
+
+void mac_pasteconsole(char *commandbuf)
+{	
     NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
     NSString *type = [pasteboard availableTypeFromArray:[NSArray arrayWithObject:NSStringPboardType]];
-    if(type != nil)
-    {
+    if (type != nil) {
         NSString *contents = [pasteboard stringForType:type];
-        if(contents != nil)
-        {
-            NSUInteger len = [contents lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1; // 10.4+
-            if(len > 1)
-            {
-                char *buf = (char *)malloc(len);
-                if(buf)
-                {
-                    if([contents getCString:buf maxLength:len encoding:NSUTF8StringEncoding]) // 10.4+
-                    {
-                        *cblen = len;
-                        return buf;
-                    }
-                    free(buf);
-                }
-            }
-        }
+        if (contents != nil)
+			s_strcat(commandbuf, [contents cStringUsingEncoding:NSASCIIStringEncoding]); // 10.4+
     }
-    return NULL;
 }
 
 /*
- * 0x1030 = 10.3
  * 0x1040 = 10.4
  * 0x1050 = 10.5
+ * 0x1060 = 10.6
  */
 int mac_osversion() 
 {
@@ -51,14 +40,14 @@ const char *mac_personaldir() {
         path = [(NSURL *)url path];
         CFRelease(url);
     }
-    return path ? copystring(dir, [path fileSystemRepresentation]) : NULL;
+    return path ? s_strcpy(dir, [path fileSystemRepresentation]) : NULL;
 }
 
 const char *mac_sauerbratendir() {
     static string dir;
     NSString *path = [[NSWorkspace sharedWorkspace] fullPathForApplication:@"sauerbraten"];
     if(path) path = [path stringByAppendingPathComponent:@"Contents/gamedata"];
-    return path ? copystring(dir, [path fileSystemRepresentation]) : NULL;
+    return path ? s_strcpy(dir, [path fileSystemRepresentation]) : NULL;
 }
 
 const char *mac_resourcedir(const char *what)
