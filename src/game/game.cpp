7002931c56e35d8347fcab44e530ff9a676f9239
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2011-12-23 09:44:34 +0000

    make buff flash white
    Former-commit-id: 2bf61907d7e1f8d1f4fff713285f5c4b328479ff
    Former-commit-id: 2fb773735e7ece1dad1beb0334a47271392e8a6d

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2442,8 +2442,8 @@ namespace game
                 flags |= MDL_LIGHTFX;
                 int millis = lastmillis%1000;
                 float amt = millis <= 500 ? 1.f-(millis/500.f) : (millis-500)/500.f;
-                flashcolour(e->light.material[1].r, e->light.material[1].g, e->light.material[1].b, uchar(64), uchar(255), uchar(255), amt);
-                flashcolour(e->light.effect.r, e->light.effect.g, e->light.effect.b, 0.25f, 1.f, 1.f, amt);
+                flashcolour(e->light.material[1].r, e->light.material[1].g, e->light.material[1].b, uchar(255), uchar(255), uchar(255), amt);
+                flashcolour(e->light.effect.r, e->light.effect.g, e->light.effect.b, 1.f, 1.f, 1.f, amt);
                 //vec col = vec(0.25f, 1.f, 1.f).mul(amt);
                 //e->light.material[1] = bvec::fromcolor(e->light.material[1].tocolor().max(col));
                 //e->light.effect.max(col);
