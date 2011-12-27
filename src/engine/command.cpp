ee96f2fc26a4303a89c627d32ed55e4962287291
Lee Salzman <lsalzman@gmail.com>
2011-12-27 00:55:52 +0000

    validatealias fix
    Former-commit-id: e6a67e826c5b2a60ffa050312923ee4a8fbea4e8
    Former-commit-id: 0ab2585b2c355445130df3a77985121d569ee03c

--- src/engine/command.cpp
+++ src/engine/command.cpp
@@ -1923,7 +1923,7 @@ bool validatealias(const char *s)
         case '/': if(s[1] == '/') return false; break;
         case '\f': return false;
     }
-    if(braks || parens) return false;
+    if(braks || parens || curly) return false;
     return true;
 }
 
