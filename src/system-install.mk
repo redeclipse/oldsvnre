9257c8b73a37be181ce249486bfc67ec383b99af
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2012-01-05 00:04:27 +0000

    variabelize redeclipse, and sed globally
    Former-commit-id: 00609ad1f71d6371bb94d84dfc8ecba7c11f2069
    Former-commit-id: 6a4ad268456ec578fe541a1c6256e4ead739ca3d

--- src/system-install.mk
+++ src/system-install.mk
@@ -9,6 +9,7 @@ ICONS= \
 prefix=/usr/local
 games=
 gamesbin=/bin
+redeclipse=redeclipse
 bindir=$(DESTDIR)$(prefix)/bin
 gamesbindir=$(DESTDIR)$(prefix)$(gamesbin)
 libexecdir=$(DESTDIR)$(prefix)/lib$(games)
@@ -33,49 +34,54 @@ install/nix/redeclipse_x128.png: redeclipse.ico
 icons: $(ICONS)
 
 system-install-client: client
-	install -d $(libexecdir)/redeclipse
+	install -d $(libexecdir)/$(redeclipse)
 	install -d $(gamesbindir)
-	install -m755 reclient $(libexecdir)/redeclipse/redeclipse
+	install -m755 reclient $(libexecdir)/$(redeclipse)/$(redeclipse)
 	install -m755 install/nix/redeclipse.am \
-		$(gamesbindir)/redeclipse
-	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),' \
-		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),' \
-		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),' \
-		-i $(gamesbindir)/redeclipse
+		$(gamesbindir)/$(redeclipse)
+	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
+		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
+		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
+		-e 's,@REDECLIPSE@,$(redeclipse),g' \
+		-i $(gamesbindir)/$(redeclipse)
 
 system-install-server: server
-	install -d $(libexecdir)/redeclipse
+	install -d $(libexecdir)/$(redeclipse)
 	install -d $(gamesbindir)
-	install -m755 reserver $(libexecdir)/redeclipse/redeclipse-server
+	install -m755 reserver \
+		$(libexecdir)/$(redeclipse)/$(redeclipse)-server
 	install -m755 install/nix/redeclipse-server.am \
-		$(gamesbindir)/redeclipse-server
-	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),' \
-		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),' \
-		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),' \
-		-i $(gamesbindir)/redeclipse-server
+		$(gamesbindir)/$(redeclipse)-server
+	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
+		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
+		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
+		-e 's,@REDECLIPSE@,$(redeclipse),g' \
+		-i $(gamesbindir)/$(redeclipse)-server
 
 system-install-data:
-	install -d $(datadir)/redeclipse
-	install -d $(libexecdir)/redeclipse
-	cp -r ../data $(datadir)/redeclipse/data
-	@rm -rv $(datadir)/redeclipse/data/examples
-	ln -s $$(readlink -f $(datadir))/redeclipse/data \
-		$$(readlink -f $(libexecdir))/redeclipse/data
+	install -d $(datadir)/$(redeclipse)
+	install -d $(libexecdir)/$(redeclipse)
+	cp -r ../data $(datadir)/$(redeclipse)/data
+	@rm -rv $(datadir)/$(redeclipse)/data/examples
+	ln -s $$(readlink -f $(datadir))/$(redeclipse)/data \
+		$$(readlink -f $(libexecdir))/$(redeclipse)/data
 
 system-install-docs: $(MANPAGES)
 	install	-d $(mandir)/man6
-	install -d $(docdir)/redeclipse
-	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),' \
-		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),' \
-		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),' \
+	install -d $(docdir)/$(redeclipse)
+	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
+		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
+		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
+		-e 's,@REDECLIPSE@,$(redeclipse),g' \
 		install/nix/redeclipse.6.am | \
-		$(GZIP) -9 -c > $(mandir)/man6/redeclipse.6.gz
-	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),' \
-		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),' \
-		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),' \
+		$(GZIP) -9 -c > $(mandir)/man6/$(redeclipse).6.gz
+	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
+		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
+		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
+		-e 's,@REDECLIPSE@,$(redeclipse),g' \
 		install/nix/redeclipse-server.6.am | \
-		$(GZIP) -9 -c > $(mandir)/man6/redeclipse-server.6.gz
-	cp -r ../data/examples $(docdir)/redeclipse/examples
+		$(GZIP) -9 -c > $(mandir)/man6/$(redeclipse)-server.6.gz
+	cp -r ../data/examples $(docdir)/$(redeclipse)/examples
 
 system-install-menus: icons
 	install -d $(menudir)
@@ -83,16 +89,20 @@ system-install-menus: icons
 	install -d $(icondir)/48x48/apps
 	install -d $(icondir)/64x64/apps
 	install -d $(icondir)/128x128/apps
-	install -m644 install/nix/redeclipse.desktop \
-		$(menudir)/redeclipse.desktop
+	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
+		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
+		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
+		-e 's,@REDECLIPSE@,$(redeclipse),g' \
+		install/nix/redeclipse.desktop.am > \
+		$(menudir)/$(redeclipse).desktop
 	install -m644 install/nix/redeclipse_x32.png \
-		$(icondir)/32x32/apps/redeclipse.png
+		$(icondir)/32x32/apps/$(redeclipse).png
 	install -m644 install/nix/redeclipse_x48.png \
-		$(icondir)/48x48/apps/redeclipse.png
+		$(icondir)/48x48/apps/$(redeclipse).png
 	install -m644 install/nix/redeclipse_x64.png \
-		$(icondir)/64x64/apps/redeclipse.png
+		$(icondir)/64x64/apps/$(redeclipse).png
 	install -m644 install/nix/redeclipse_x128.png \
-		$(icondir)/128x128/apps/redeclipse.png
+		$(icondir)/128x128/apps/$(redeclipse).png
 
 system-install-cube2font: system-install-cube2font-docs
 	install -d $(bindir)
@@ -106,33 +116,33 @@ system-install-cube2font-docs: install/nix/cube2font.1
 system-install: system-install-client system-install-server system-install-data system-install-docs system-install-menus
 
 system-uninstall-client:
-	@rm -fv $(libexecdir)/redeclipse/redeclipse
-	@rm -fv $(gamesbindir)/redeclipse
+	@rm -fv $(libexecdir)/$(redeclipse)/$(redeclipse)
+	@rm -fv $(gamesbindir)/$(redeclipse)
 
 system-uninstall-server:
-	@rm -fv $(libexecdir)/redeclipse/redeclipse-server
-	@rm -fv $(gamesbindir)/redeclipse-server
+	@rm -fv $(libexecdir)/$(redeclipse)/$(redeclipse)-server
+	@rm -fv $(gamesbindir)/$(redeclipse)-server
 
 system-uninstall-data:
-	rm -rf $(datadir)/redeclipse/data
-	@rm -fv $(libexecdir)/redeclipse/data
+	rm -rf $(datadir)/$(redeclipse)/data
+	@rm -fv $(libexecdir)/$(redeclipse)/data
 
 system-uninstall-docs:
-	@rm -rfv $(docdir)/redeclipse/examples
-	@rm -fv $(mandir)/man6/redeclipse.6.gz
-	@rm -fv $(mandir)/man6/redeclipse-server.6.gz
+	@rm -rfv $(docdir)/$(redeclipse)/examples
+	@rm -fv $(mandir)/man6/$(redeclipse).6.gz
+	@rm -fv $(mandir)/man6/$(redeclipse)-server.6.gz
 
 system-uninstall-menus:
-	@rm -fv $(menudir)/redeclipse.desktop
-	@rm -fv $(icondir)/32x32/apps/redeclipse.png
-	@rm -fv $(icondir)/48x48/apps/redeclipse.png
-	@rm -fv $(icondir)/64x64/apps/redeclipse.png
-	@rm -fv $(icondir)/128x128/apps/redeclipse.png
+	@rm -fv $(menudir)/$(redeclipse).desktop
+	@rm -fv $(icondir)/32x32/apps/$(redeclipse).png
+	@rm -fv $(icondir)/48x48/apps/$(redeclipse).png
+	@rm -fv $(icondir)/64x64/apps/$(redeclipse).png
+	@rm -fv $(icondir)/128x128/apps/$(redeclipse).png
 
 system-uninstall: system-uninstall-client system-uninstall-server system-uninstall-data system-uninstall-docs system-uninstall-menus
-	-@rmdir -v $(libexecdir)/redeclipse
-	-@rmdir -v $(datadir)/redeclipse
-	-@rmdir -v $(docdir)/redeclipse
+	-@rmdir -v $(libexecdir)/$(redeclipse)
+	-@rmdir -v $(datadir)/$(redeclipse)
+	-@rmdir -v $(docdir)/$(redeclipse)
 
 system-uninstall-cube2font-docs:
 	@rm -fv $(mandir)/man1/cube2font.1.gz
