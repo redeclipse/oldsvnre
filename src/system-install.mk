c89593c84586fd289f686bc644812407fe85189d
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2012-06-20 02:59:27 +0000

    more name abstractions for easy diff'ing
    Former-commit-id: a572a330dc38ba25cdf97629a7db96ebc8a24a2d
    Former-commit-id: cc2b4cda378522b90a4b72803a35a5b8a9edd6b7

--- src/system-install.mk
+++ src/system-install.mk
@@ -1,16 +1,7 @@
-GZIPPER=gzip
-
-ICONS= \
-	install/nix/redeclipse_x16.png \
-	install/nix/redeclipse_x32.png \
-	install/nix/redeclipse_x48.png \
-	install/nix/redeclipse_x64.png \
-	install/nix/redeclipse_x128.png
-
+appname=redeclipse
 prefix=/usr/local
 games=
 gamesbin=/bin
-redeclipse=redeclipse
 bindir=$(DESTDIR)$(prefix)/bin
 gamesbindir=$(DESTDIR)$(prefix)$(gamesbin)
 libexecdir=$(DESTDIR)$(prefix)/lib$(games)
@@ -20,71 +11,80 @@ mandir=$(DESTDIR)$(prefix)/share/man
 menudir=$(DESTDIR)$(prefix)/share/applications
 icondir=$(DESTDIR)$(prefix)/share/icons/hicolor
 
-install/nix/redeclipse_x16.png: redeclipse.ico
-	convert 'redeclipse.ico[0]' $@
+GZIPPER=gzip
 
-install/nix/redeclipse_x32.png: redeclipse.ico
-	convert 'redeclipse.ico[1]' $@
+ICONS= \
+	install/nix/$(appname)_x16.png \
+	install/nix/$(appname)_x32.png \
+	install/nix/$(appname)_x48.png \
+	install/nix/$(appname)_x64.png \
+	install/nix/$(appname)_x128.png
 
-install/nix/redeclipse_x48.png: redeclipse.ico
-	convert 'redeclipse.ico[2]' -resize 48x48 $@
+install/nix/$(appname)_x16.png: $(appname).ico
+	convert '$(appname).ico[0]' $@
 
-install/nix/redeclipse_x64.png: redeclipse.ico
-	convert 'redeclipse.ico[2]' $@
+install/nix/$(appname)_x32.png: $(appname).ico
+	convert '$(appname).ico[1]' $@
 
-install/nix/redeclipse_x128.png: redeclipse.ico
-	convert 'redeclipse.ico[3]' $@
+install/nix/$(appname)_x48.png: $(appname).ico
+	convert '$(appname).ico[2]' -resize 48x48 $@
+
+install/nix/$(appname)_x64.png: $(appname).ico
+	convert '$(appname).ico[2]' $@
+
+install/nix/$(appname)_x128.png: $(appname).ico
+	convert '$(appname).ico[3]' $@
 
 icons: $(ICONS)
 
 system-install-client: client
-	install -d $(libexecdir)/$(redeclipse)
+	install -d $(libexecdir)/$(appname)
 	install -d $(gamesbindir)
-	install -m755 reclient $(libexecdir)/$(redeclipse)/$(redeclipse)
-	install -m755 install/nix/redeclipse.am \
-		$(gamesbindir)/$(redeclipse)
+	install -m755 reclient $(libexecdir)/$(appname)/$(appname)
+	install -m755 install/nix/$(appname).am \
+		$(gamesbindir)/$(appname)
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
-		-e 's,@REDECLIPSE@,$(redeclipse),g' \
-		-i $(gamesbindir)/$(redeclipse)
-	ln -s $(patsubst $(DESTDIR)%,%,$(datadir))/$(redeclipse)/data \
-		$(libexecdir)/$(redeclipse)/data
+		-e 's,@$(appname)@,$(appname),g' \
+		-i $(gamesbindir)/$(appname)
+	ln -s $(patsubst $(DESTDIR)%,%,$(datadir))/$(appname)/data \
+		$(libexecdir)/$(appname)/data
 
 system-install-server: server
-	install -d $(libexecdir)/$(redeclipse)
+	install -d $(libexecdir)/$(appname)
 	install -d $(gamesbindir)
 	install -m755 reserver \
-		$(libexecdir)/$(redeclipse)/$(redeclipse)-server
-	install -m755 install/nix/redeclipse-server.am \
-		$(gamesbindir)/$(redeclipse)-server
+		$(libexecdir)/$(appname)/$(appname)-server
+	install -m755 install/nix/$(appname)-server.am \
+		$(gamesbindir)/$(appname)-server
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
-		-e 's,@REDECLIPSE@,$(redeclipse),g' \
-		-i $(gamesbindir)/$(redeclipse)-server
+		-e 's,@$(appname)@,$(appname),g' \
+		-i $(gamesbindir)/$(appname)-server
 
 system-install-data:
-	install -d $(datadir)/$(redeclipse)
-	cp -r ../data $(datadir)/$(redeclipse)/data
-	@rm -rv $(datadir)/$(redeclipse)/data/examples
+	install -d $(datadir)/$(appname)
+	cp -r ../data $(datadir)/$(appname)/data
+	@rm -rv $(datadir)/$(appname)/data/examples
 
 system-install-docs: $(MANPAGES)
 	install	-d $(mandir)/man6
-	install -d $(docdir)/$(redeclipse)
+	install -d $(docdir)/$(appname)
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
-		-e 's,@REDECLIPSE@,$(redeclipse),g' \
-		install/nix/redeclipse.6.am | \
-		$(GZIPPER) -9 -n -c > $(mandir)/man6/$(redeclipse).6.gz
+		-e 's,@$(appname)@,$(appname),g' \
+		install/nix/$(appname).6.am | \
+		$(GZIPPER) -9 -n -c > $(mandir)/man6/$(appname).6.gz
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
-		-e 's,@REDECLIPSE@,$(redeclipse),g' \
-		install/nix/redeclipse-server.6.am | \
-		$(GZIPPER) -9 -n -c > $(mandir)/man6/$(redeclipse)-server.6.gz
-	cp -r ../data/examples $(docdir)/$(redeclipse)/examples
+		-e 's,@$(appname)@,$(appname),g' \
+		install/nix/$(appname)-server.6.am | \
+		$(GZIPPER) -9 -n -c > $(mandir)/man6/$(appname)-server.6.gz
+	cp -r ../data/examples $(docdir)/$(appname)/examples
 
 system-install-menus: icons
 	install -d $(menudir)
@@ -96,19 +96,19 @@ system-install-menus: icons
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
-		-e 's,@REDECLIPSE@,$(redeclipse),g' \
-		install/nix/redeclipse.desktop.am > \
-		$(menudir)/$(redeclipse).desktop
-	install -m644 install/nix/redeclipse_x16.png \
-		$(icondir)/16x16/apps/$(redeclipse).png
-	install -m644 install/nix/redeclipse_x32.png \
-		$(icondir)/32x32/apps/$(redeclipse).png
-	install -m644 install/nix/redeclipse_x48.png \
-		$(icondir)/48x48/apps/$(redeclipse).png
-	install -m644 install/nix/redeclipse_x64.png \
-		$(icondir)/64x64/apps/$(redeclipse).png
-	install -m644 install/nix/redeclipse_x128.png \
-		$(icondir)/128x128/apps/$(redeclipse).png
+		-e 's,@$(appname)@,$(appname),g' \
+		install/nix/$(appname).desktop.am > \
+		$(menudir)/$(appname).desktop
+	install -m644 install/nix/$(appname)_x16.png \
+		$(icondir)/16x16/apps/$(appname).png
+	install -m644 install/nix/$(appname)_x32.png \
+		$(icondir)/32x32/apps/$(appname).png
+	install -m644 install/nix/$(appname)_x48.png \
+		$(icondir)/48x48/apps/$(appname).png
+	install -m644 install/nix/$(appname)_x64.png \
+		$(icondir)/64x64/apps/$(appname).png
+	install -m644 install/nix/$(appname)_x128.png \
+		$(icondir)/128x128/apps/$(appname).png
 
 system-install-cube2font: system-install-cube2font-docs
 	install -d $(bindir)
@@ -122,34 +122,34 @@ system-install-cube2font-docs: install/nix/cube2font.1
 system-install: system-install-client system-install-server system-install-data system-install-docs system-install-menus
 
 system-uninstall-client:
-	@rm -fv $(libexecdir)/$(redeclipse)/$(redeclipse)
-	@rm -fv $(libexecdir)/$(redeclipse)/data
-	@rm -fv $(gamesbindir)/$(redeclipse)
+	@rm -fv $(libexecdir)/$(appname)/$(appname)
+	@rm -fv $(libexecdir)/$(appname)/data
+	@rm -fv $(gamesbindir)/$(appname)
 
 system-uninstall-server:
-	@rm -fv $(libexecdir)/$(redeclipse)/$(redeclipse)-server
-	@rm -fv $(gamesbindir)/$(redeclipse)-server
+	@rm -fv $(libexecdir)/$(appname)/$(appname)-server
+	@rm -fv $(gamesbindir)/$(appname)-server
 
 system-uninstall-data:
-	rm -rf $(datadir)/$(redeclipse)/data
+	rm -rf $(datadir)/$(appname)/data
 
 system-uninstall-docs:
-	@rm -rfv $(docdir)/$(redeclipse)/examples
-	@rm -fv $(mandir)/man6/$(redeclipse).6.gz
-	@rm -fv $(mandir)/man6/$(redeclipse)-server.6.gz
+	@rm -rfv $(docdir)/$(appname)/examples
+	@rm -fv $(mandir)/man6/$(appname).6.gz
+	@rm -fv $(mandir)/man6/$(appname)-server.6.gz
 
 system-uninstall-menus:
-	@rm -fv $(menudir)/$(redeclipse).desktop
-	@rm -fv $(icondir)/16x16/apps/$(redeclipse).png
-	@rm -fv $(icondir)/32x32/apps/$(redeclipse).png
-	@rm -fv $(icondir)/48x48/apps/$(redeclipse).png
-	@rm -fv $(icondir)/64x64/apps/$(redeclipse).png
-	@rm -fv $(icondir)/128x128/apps/$(redeclipse).png
+	@rm -fv $(menudir)/$(appname).desktop
+	@rm -fv $(icondir)/16x16/apps/$(appname).png
+	@rm -fv $(icondir)/32x32/apps/$(appname).png
+	@rm -fv $(icondir)/48x48/apps/$(appname).png
+	@rm -fv $(icondir)/64x64/apps/$(appname).png
+	@rm -fv $(icondir)/128x128/apps/$(appname).png
 
 system-uninstall: system-uninstall-client system-uninstall-server system-uninstall-data system-uninstall-docs system-uninstall-menus
-	-@rmdir -v $(libexecdir)/$(redeclipse)
-	-@rmdir -v $(datadir)/$(redeclipse)
-	-@rmdir -v $(docdir)/$(redeclipse)
+	-@rmdir -v $(libexecdir)/$(appname)
+	-@rmdir -v $(datadir)/$(appname)
+	-@rmdir -v $(docdir)/$(appname)
 
 system-uninstall-cube2font-docs:
 	@rm -fv $(mandir)/man1/cube2font.1.gz
