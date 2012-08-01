9b45e01ce2efb9756fef67a5eeedbfc7cdcfaacf
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2012-08-01 11:35:25 +0000

    dist make targets for creating linux_bsd tarballs
    Currently does not include lib/ or include/ in tarball, should or not?
    
    
    Former-commit-id: bedd70db2dfbed33f2099818cece533d1a5fb7bb
    Former-commit-id: 77b9c36fde6aee7c696d35a1874175958d74e070

---
+++ src/dist.mk
@@ -0,0 +1,47 @@
+appversion=$(shell sed -n 's,.*RE_VER_STR.*"\(.*\)",\1,p' engine/engine.h)
+dirname=$(APPNAME)-$(appversion)
+tarname=$(APPNAME)_$(appversion)_linux_bsd.tar
+
+../$(dirname):
+	rm -rf ../$(dirname)
+	tar \
+		--exclude-vcs --exclude-backups \
+		--exclude='../$(dirname)' --exclude='../$(tarname)*' \
+		--exclude='*.o' --exclude='*.lo' --exclude='*.gch' \
+		--exclude='*src/reclient' --exclude='*src/reserver' \
+		--exclude='*.exe' --exclude='*.dll' \
+		--exclude='*redeclipse.app*' --exclude='*.bat' \
+		--exclude='*src/lib*' --exclude='*src/include*' \
+		--exclude='*src/xcode*' --exclude='*src/site*' \
+		-cf - ../ | (mkdir ../$(dirname)/; cd ../$(dirname)/ ; tar -xpf -)
+	$(MAKE) -C ../$(dirname)/src clean
+	-$(MAKE) -C ../$(dirname)/src/enet distclean
+	rm -rf ../$(dirname)/src/enet/autom4te.cache/
+
+distdir: ../$(dirname)
+
+../$(tarname): ../$(dirname)
+	tar -cf ../$(tarname) ../$(dirname)
+
+dist-tar: ../$(tarname)
+
+dist-gz: ../$(tarname)
+	gzip -c < ../$(tarname) > ../$(tarname).gz
+
+dist-bz2: ../$(tarname)
+	bzip2 -c < ../$(tarname) > ../$(tarname).bz2
+
+dist-xz: ../$(tarname)
+	xz -c < ../$(tarname) > ../$(tarname).xz
+
+dist: dist-bz2
+
+dist-all: dist-gz dist-bz2 dist-xz
+
+dist-clean:
+	rm -rf ../$(dirname)
+	rm -f ../$(tarname)
+	rm -f ../$(tarname).gz
+	rm -f ../$(tarname).bz2
+	rm -f ../$(tarname).xz
+
