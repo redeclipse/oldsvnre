74eb545ebf571e0273d07556658e2194e80cbd7f
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2012-08-13 11:20:40 +0000

    Move generation of cube2font.txt to makefile
    Former-commit-id: 25ed9ae24db22220d316da87dd4f3b511e5323e7
    Former-commit-id: d1542bd0d5f9d97d09961b03bee89159a9981cae

--- doc/generate_cube2font_txt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-man --no-justification --no-hyphenation man/cube2font.1 | col -b > cube2font.txt
-
-cat >> $(dirname $0)/cube2font.txt << EOF
-
-This text file was automatically generated from cube2font.1
-Please do not edit it manually.
-EOF
-
