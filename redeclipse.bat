e0ced604cd550c1f209feba0ce3824a44916e40d
Quinton Reeves <qreeves@gmail.com>
2012-08-26 01:37:20 +0000

    more batch file tweaks
    Former-commit-id: 6ea977d66768b8a0028dc6390130b5eb627c3a3c
    Former-commit-id: 9022d9b891b7a67a22fc53abbb49f3165e4d725f

--- redeclipse.bat
+++ redeclipse.bat
@@ -5,8 +5,12 @@ set RE_DIR=.
 set RE_OPTIONS=-glog.txt
 set RE_BINARY=bin
 
-IF %PROCESSOR_ARCHITECTURE% == amd64 set RE_BINARY=bin64
-IF %PROCESSOR_ARCHITEW6432% == amd64 set RE_BINARY=bin64
+IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+    set RE_BINARY=bin64
+)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+    set RE_BINARY=bin64
+)
 
 :RETRY
 IF EXIST %RE_BINARY%\reclient.exe (
