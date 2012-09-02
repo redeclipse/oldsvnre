19737540fb4d7951c97df37bacce4681082616bc
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2012-09-02 14:06:56 +0000

    dist.mk: Add dist-torrents target, revamp dist target
    * torrent targets to create torrents from dist products
    * make dist targets into files
    * win target with separate dir (avoids causing rebuilds from main dir)
    * dist-mostlyclean removes intermediate tars and dirs
    
    
    Former-commit-id: cbf923098e7a00d72d193d213f47cc80c46b3d17
    Former-commit-id: 99f03d01e028369c9c468180684888a30a2855da

--- src/dist.mk
+++ src/dist.mk
@@ -1,9 +1,12 @@
 appversion=$(shell sed -n 's,.*RE_VER_STR.*"\(.*\)",\1,p' engine/engine.h)
 dirname=$(APPNAME)-$(appversion)
+dirname-win=$(APPNAME)-$(appversion)-win
 tarname=$(APPNAME)_$(appversion)_nix_bsd.tar
 tarname-all=$(APPNAME)_$(appversion)_all.tar
 tarname-osx=$(APPNAME)_$(appversion)_osx.tar
 exename=$(APPNAME)_$(appversion)_win.exe
+torrent-trackers-url="udp://tracker.openbittorrent.com:80,udp://tracker.publicbt.com:80,udp://tracker.ccc.de:80,udp://tracker.istole.it:80"
+torrent-webseed-baseurl="http://downloads.sourceforge.net/redeclipse"
 
 ../$(dirname):
 	rm -rf $@
@@ -12,6 +15,10 @@ exename=$(APPNAME)_$(appversion)_win.exe
 		--exclude='$@*' \
 		--exclude='../$(tarname)*' --exclude='../$(tarname-all)*' \
 		--exclude='../$(tarname-osx)*' --exclude='../$(exename)' \
+		--exclude='../$(torrentname)*' \
+		--exclude='../$(torrentname-all)*' \
+		--exclude='../$(torrentname-osx)*' \
+		--exclude='../$(torrentname-win)*' \
 		--exclude='*.o' --exclude='*.lo' --exclude='*.gch' \
 		--exclude='../src/reclient' --exclude='../src/reserver' \
 		--exclude='../src/site*' \
@@ -51,47 +58,125 @@ dist-tar-all: ../$(tarname-all)
 
 dist-tar-osx: ../$(tarname-osx)
 
-dist-gz: ../$(tarname)
-	gzip -f $<
+../$(tarname).gz: ../$(tarname)
+	gzip -c < $< > $@
 
-dist-bz2: ../$(tarname)
-	bzip2 -f $<
+dist-gz: ../$(tarname).gz
 
-dist-xz: ../$(tarname)
-	xz -f $<
+../$(tarname).bz2: ../$(tarname)
+	bzip2 -c < $< > $@
 
-dist-gz-all: ../$(tarname-all)
-	gzip -f $<
+dist-bz2: ../$(tarname).bz2
 
-dist-bz2-all: ../$(tarname-all)
-	bzip2 -f $<
+../$(tarname).xz: ../$(tarname)
+	xz -c < $< > $@
 
-dist-xz-all: ../$(tarname-all)
-	xz -f $<
+dist-xz: ../$(tarname).xz
 
-dist-gz-osx: ../$(tarname-osx)
-	gzip -f $<
+../$(tarname-all).gz: ../$(tarname-all)
+	gzip -c < $< > $@
 
-dist-bz2-osx: ../$(tarname-osx)
-	bzip2 -f $<
+dist-gz-all: ../$(tarname-all).gz
 
-dist-xz-osx: ../$(tarname-osx)
-	xz -f $<
+../$(tarname-all).bz2: ../$(tarname-all)
+	bzip2 -c < $< > $@
 
-../$(exename): ../$(dirname)
-	rm -rf $</bin*/redeclipse.app/
-	rm -rf $</bin*/*linux*
-	rm -rf $</bin*/*freebsd*
+dist-bz2-all: ../$(tarname-all).bz2
+
+../$(tarname-all).xz: ../$(tarname-all)
+	xz -c < $< > $@
+
+dist-xz-all: ../$(tarname-all).xz
+
+../$(tarname-osx).gz: ../$(tarname-osx)
+	gzip -c < $< > $@
+
+dist-gz-osx: ../$(tarname-osx).gz
+
+../$(tarname-osx).bz2: ../$(tarname-osx)
+	bzip2 -c < $< > $@
+
+dist-bz2-osx: ../$(tarname-osx).bz2
+
+../$(tarname-osx).xz: ../$(tarname-osx)
+	xz -c < $< > $@
+
+dist-xz-osx: ../$(tarname-osx).xz
+
+../$(dirname-win): ../$(dirname)
+	cp -r $< $@
+	rm -rf $@/bin*/redeclipse.app/
+	rm -rf $@/bin*/*linux*
+	rm -rf $@/bin*/*freebsd*
+
+distdir-win: ../$(dirname-win)
+
+../$(exename): ../$(dirname-win)
 	makensis $</src/install/win/redeclipse.nsi
 	mv $</src/install/win/$(exename) ../
-	cp -r ../bin* $<
 
 dist-win: ../$(exename)
 
 dist: dist-bz2 dist-bz2-all dist-bz2-osx dist-win
 
-dist-clean:
+../$(tarname).bz2.torrent: ../$(tarname)
+	rm -f $@
+	cd ../ &&\
+		mktorrent \
+		-a $(torrent-trackers-url) \
+		-w $(torrent-webseed-baseurl)/$(tarname).bz2 \
+		-n $(tarname).bz2 \
+		-c "Red Eclipse $(appversion) for Linux and BSD" \
+		$(tarname).bz2
+
+dist-torrent: ../$(tarname).bz2.torrent
+
+../$(tarname-all).bz2.torrent: ../$(tarname-all)
+	rm -f $@
+	cd ../ &&\
+		mktorrent \
+		-a $(torrent-trackers-url) \
+		-w $(torrent-webseed-baseurl)/$(tarname-all).bz2 \
+		-n $(tarname-all).bz2 \
+		-c "Red Eclipse $(appversion) for All Platforms" \
+		$(tarname-all).bz2
+
+dist-torrent-all: ../$(tarname-all).bz2.torrent
+
+../$(tarname-osx).bz2.torrent: ../$(tarname-osx)
+	rm -f $@
+	cd ../ &&\
+		mktorrent \
+		-a $(torrent-trackers-url) \
+		-w $(torrent-webseed-baseurl)/$(tarname-osx).bz2 \
+		-n $(tarname-osx).bz2 \
+		-c "Red Eclipse $(appversion) for OSX" \
+		$(tarname-osx).bz2
+
+dist-torrent-osx: ../$(tarname-osx).bz2.torrent
+
+../$(exename).torrent: ../$(exename)
+	rm -f $@
+	cd ../ &&\
+		mktorrent \
+		-a $(torrent-trackers-url) \
+		-w $(torrent-webseed-baseurl)/$(exename) \
+		-n $(exename) \
+		-c "Red Eclipse $(appversion) for Windows" \
+		$(exename)
+
+dist-torrent-win: ../$(exename).torrent
+
+dist-torrents: dist-torrent dist-torrent-all dist-torrent-osx dist-torrent-win
+
+dist-mostlyclean:
 	rm -rf ../$(dirname)
+	rm -rf ../$(dirname-win)
+	rm -f ../$(tarname)
+	rm -f ../$(tarname-all)
+	rm -f ../$(tarname-osx)
+
+dist-clean: dist-mostlyclean
 	rm -f ../$(tarname)*
 	rm -f ../$(tarname-all)*
 	rm -f ../$(tarname-osx)*
