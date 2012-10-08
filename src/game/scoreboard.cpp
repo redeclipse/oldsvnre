ce3232db16d1c85e7a55055d3630c2c7c65f3bde
Quinton Reeves <qreeves@gmail.com>
2012-10-08 07:05:30 +0000

    fix name padding
    Former-commit-id: b0fff0cb8f859f956f346c5c910118fb14050082
    Former-commit-id: e208e6a018e9d3394ac3a9ea080ca78ab78a905a

--- src/game/scoreboard.cpp
+++ src/game/scoreboard.cpp
@@ -377,9 +377,9 @@ namespace hud
             loopk(numgroups)
             {
                 scoregroup &sg = *groups[k];
-                loopscoregroup(namepad = max(namepad, text_width(game::colorname(o, NULL, "", false))*0.5f/guibound[0]));
+                loopscoregroup(namepad = max(namepad, (float)text_width(game::colorname(o, NULL, "", false))));
             }
-            namepad -= (text_width("name")-1)*0.5f/guibound[0];
+            namepad = (namepad-(text_width("name")-guibound[0]))*0.5f/guibound[0];
             loopk(numgroups)
             {
                 if((k%2)==0)
