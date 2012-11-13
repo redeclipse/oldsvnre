61236d8e611a585995b2d1393d58e4796f688124
Lee Salzman <lsalzman@gmail.com>
2012-11-13 04:30:53 +0000

    nocolorglsl tweaks
    Former-commit-id: cd39bbc5c03bcbb892140aeee31d1a71bebce8bf
    Former-commit-id: df08ffe8a5e7690b463d0013846332137d32183f

--- src/engine/texture.h
+++ src/engine/texture.h
@@ -762,7 +762,7 @@ struct cubemapside
 
 extern cubemapside cubemapsides[6];
 extern Texture *notexture, *blanktexture;
-extern Shader *defaultshader, *rectshader, *cubemapshader, *notextureshader, *nocolorshader, *foggedshader, *foggednotextureshader, *stdworldshader, *lineshader, *foggedlineshader;
+extern Shader *defaultshader, *rectshader, *cubemapshader, *notextureshader, *nocolorshader, *nocolorglslshader, *foggedshader, *foggednotextureshader, *stdworldshader, *lineshader, *foggedlineshader;
 extern int reservevpparams, maxvpenvparams, maxvplocalparams, maxfpenvparams, maxfplocalparams, maxvsuniforms, maxfsuniforms;
 
 extern Shader *lookupshaderbyname(const char *name);
