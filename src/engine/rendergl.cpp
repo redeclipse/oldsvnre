c1e3863611d1c5f36364a9888845643a82f91b48
Lee Salzman <lsalzman@gmail.com>
2013-01-04 02:43:14 +0000

    fix avoidshaders message
    Former-commit-id: dc4f604a306c4d458e1b88562d55d59f3b76686b
    Former-commit-id: 72402bf9580846aad78a3415da10dad3fbcd2c4b

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -784,7 +784,7 @@ void gl_init(int w, int h, int bpp, int depth, int fsaa)
     if(!useshaders || (useshaders<0 && avoidshaders) || !hasMT || !hasshaders)
     {
         if(!hasMT || !hasshaders) conoutf("\frWARNING: No shader support! Using fixed-function fallback. (no fancy visuals for you)");
-        else if(useshaders<0 && !hasTF) conoutf("\frWARNING: Disabling shaders for extra performance. (use \"/shaders 1\" to enable shaders if desired)");
+        else if(useshaders<0 && avoidshaders) conoutf("\frWARNING: Disabling shaders for extra performance. (use \"/shaders 1\" to enable shaders if desired)");
         renderpath = R_FIXEDFUNCTION;
     }
     else renderpath = hasGLSL ? ((forceglsl && (forceglsl > 0 || preferglsl)) || !hasVP || !hasFP ? (forceglsl ? R_GLSLANG : R_FIXEDFUNCTION) : R_ASMGLSLANG) : R_ASMSHADER;
