c6a434c5fb1ed6fb9031a7509875c64600c4b6ad
Quinton Reeves <qreeves@gmail.com>
2013-01-08 14:57:32 +0000

    fix typo
    Former-commit-id: d73fa18d55b109b721e26e141c256b6de587eb71
    Former-commit-id: 0400d72278e577e606dbe8944bf0e6fd70683c2f

--- src/game/weapdef.h
+++ src/game/weapdef.h
@@ -1,4 +1,4 @@
-#ifdef GAMSERVER
+#ifdef GAMESERVER
     #define WPVAR(flags, name, mn, mx, w00, w01, w02, w03, w04, w05, w06, w07, w08, w09, w10) \
         GVAR(flags, melee##name, mn, w00, mx); \
         GVAR(flags, pistol##name, mn, w01, mx); \
