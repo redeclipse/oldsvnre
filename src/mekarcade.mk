139b8566495f8a68947b871f220f58e975911914
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-02-14 19:16:20 +0000

    makefiles: move stuff from top to mekarcade.mk
    Former-commit-id: 632d17664bb242c63c5557bf93116caa1356b894
    Former-commit-id: d37fd98ff1aaced116b3e271abf3483bf2f192c5

--- src/mekarcade.mk
+++ src/mekarcade.mk
@@ -1,6 +1,18 @@
+ifeq ($(APPNAME),mekarcade)
 APPFLAGS= -DMEK=1
 APPNAME=mekarcade
 APPCLIENT=mekclient
 APPSERVER=mekserver
+endif
 
-include core.mk
+MEK_DEFINES= \
+	APPNAME=mekarcade \
+	APPCLIENT=mekclient \
+	APPSERVER=mekserver \
+	APPFLAGS=" -DMEK=1"
+
+mekarcade:
+	$(MAKE) $(MEK_DEFINES)
+
+install-mekarcade:
+	$(MAKE) $(MEK_DEFINES) install
