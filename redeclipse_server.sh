fd409921ff87ef7e7533c771fcf6a55cacafc01c
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-03-07 14:29:20 +0000

    launcher scripts: consistency, fix spaces in path
    * Quotes quotes and quotes (fixes paths with spaces)
    * Use ${} only when building strings
    * Use $() instead of ``
    
    
    Former-commit-id: 0f23697febbedc7042475afba65b1d2ddc78bd4c
    Former-commit-id: e9ecaa9cab1a33327eeff00b6acb75504249b43b

--- redeclipse_server.sh
+++ redeclipse_server.sh
@@ -9,18 +9,18 @@ RE_OPTIONS=""
 
 # SYSTEM_NAME should be set to the name of your operating system.
 #SYSTEM_NAME=Linux
-SYSTEM_NAME=`uname -s`
+SYSTEM_NAME="$(uname -s)"
 
 # MACHINE_NAME should be set to the name of your processor.
 #MACHINE_NAME=i686
-MACHINE_NAME=`uname -m`
+MACHINE_NAME="$(uname -m)"
 
-if [ -x ${RE_PATH}/bin/redeclipse_server_native ]
+if [ -x "${RE_PATH}/bin/redeclipse_server_native" ]
 then
     SYSTEM_SUFFIX="_native"
     RE_ARCH=""
 else
-    case ${SYSTEM_NAME} in
+    case "$SYSTEM_NAME" in
     Linux)
         SYSTEM_SUFFIX="_linux"
         ;;
@@ -32,7 +32,7 @@ else
         ;;
     esac
 
-    case ${MACHINE_NAME} in
+    case "$MACHINE_NAME" in
     i486|i586|i686)
         RE_ARCH="x86/"
         ;;
@@ -46,16 +46,16 @@ else
     esac
 fi
 
-if [ -x ${RE_PATH}/bin/${RE_ARCH}redeclipse_server${SYSTEM_SUFFIX} ]
+if [ -x "${RE_PATH}/bin/${RE_ARCH}redeclipse_server${SYSTEM_SUFFIX}" ]
 then
-    cd ${RE_PATH} || exit 1
-    exec ${RE_PATH}/bin/${RE_ARCH}redeclipse_server${SYSTEM_SUFFIX} ${RE_OPTIONS} "$@"
+    cd "$RE_PATH" || exit 1
+    exec "${RE_PATH}/bin/${RE_ARCH}redeclipse_server${SYSTEM_SUFFIX}" $RE_OPTIONS "$@"
 else
     echo "Your platform does not have a pre-compiled Red Eclipse server."
     echo -n "Would you like to build one now? [Yn] "
     read CC
-    if [ "${CC}" != "n" ]; then
-        cd ${RE_PATH}/src || exit 1
+    if [ "$CC" != "n" ]; then
+        cd "${RE_PATH}/src" || exit 1
         make clean install-server
         echo "Build complete, please try running the script again."
     else
