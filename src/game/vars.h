156914c788eaa7deb6f61c2a393b8a33a61e395a
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-03-08 22:52:48 +0000

    fixed #426 (Classic mutator makes gauntlet pistol-only): gauntlet now spawns you with a rifle in classic
    Former-commit-id: efda14183d36559276edfd5ef1484695e65b982e
    Former-commit-id: e8e003a02cb6281ece732dc06ce8b29fa87479c2

--- src/game/vars.h
+++ src/game/vars.h
@@ -145,17 +145,17 @@ GVAR(0, maxcarry, 1, 2, W_LOADOUT);
 GVAR(0, spawnrotate, 0, 2, 2); // 0 = let client decide, 1 = sequence, 2 = random
 GVAR(0, spawnweapon, 0, W_PISTOL, W_MAX-1);
 GVAR(0, instaweapon, 0, W_RIFLE, W_MAX-1);
+GVAR(0, trialweapon, 0, W_MELEE, W_MAX-1);
+GVAR(0, gauntletweapon, 0, W_RIFLE, W_MAX-1);
 GVAR(0, spawngrenades, 0, 0, 2); // 0 = never, 1 = all but insta/trial, 2 = always
 GVAR(0, spawnmines, 0, 0, 2); // 0 = never, 1 = all but insta/trial, 2 = always
 GVAR(0, spawndelay, 0, 5000, VAR_MAX); // delay before spawning in most modes
 GVAR(0, instadelay, 0, 3000, VAR_MAX); // .. in instagib matches
+GVAR(0, trialdelay, 0, 500, VAR_MAX); // .. in time trial matches
 GVAR(0, bomberdelay, 0, 3000, VAR_MAX); // delay before spawning in bomber
 GVAR(0, spawnprotect, 0, 3000, VAR_MAX); // delay before damage can be dealt to spawning player
 GVAR(0, duelprotect, 0, 5000, VAR_MAX); // .. in duel/survivor matches
 GVAR(0, instaprotect, 0, 3000, VAR_MAX); // .. in instagib matches
-
-GVAR(0, trialweapon, 0, W_MELEE, W_MAX-1);
-GVAR(0, trialdelay, 0, 500, VAR_MAX); // .. in time trial matches
 
 #ifdef CAMPAIGN
 GVAR(0, campaignghost, 0, 0, 1); // 0 = all players are solid, 1 = all players are ghosts
