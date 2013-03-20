0c63934757f657615638aaabcf154998782ec0f2
Quinton Reeves <qreeves@gmail.com>
2013-03-20 18:56:08 +0000

    fixed mapbalance for non-affinity scenarios
    Former-commit-id: 2d2e0ca1ad04e69e95fa81782423f8efdf9d3c82
    Former-commit-id: aaa848b663a6a59e244d7e2fc697c2645514741e

--- src/game/capturemode.h
+++ src/game/capturemode.h
@@ -80,7 +80,7 @@ struct captureservmode : capturestate, servmode
                         int score = addscore(ci->team);
                         sendf(-1, 1, "ri5", N_SCOREAFFIN, ci->clientnum, i, k, score);
                         mutate(smuts, mut->scoreaffinity(ci));
-                        if(!m_balance(gamemode) && G(capturelimit) && score >= G(capturelimit))
+                        if(!m_balance(gamemode, mutators) && G(capturelimit) && score >= G(capturelimit))
                         {
                             ancmsgft(-1, S_V_NOTIFY, CON_EVENT, "\fyscore limit has been reached");
                             startintermission();
@@ -151,7 +151,7 @@ struct captureservmode : capturestate, servmode
                     int score = addscore(ci->team);
                     sendf(-1, 1, "ri5", N_SCOREAFFIN, ci->clientnum, i, -1, score);
                     mutate(smuts, mut->scoreaffinity(ci));
-                    if(!m_balance(gamemode) && G(capturelimit) && score >= G(capturelimit))
+                    if(!m_balance(gamemode, mutators) && G(capturelimit) && score >= G(capturelimit))
                     {
                         ancmsgft(-1, S_V_NOTIFY, CON_EVENT, "\fyscore limit has been reached");
                         startintermission();
