a111b6f23ec9995075b0bcb156769b23fbcedce8
arand-2 <arand-2@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-03-23 11:33:11 +0000

    system-install: use mkdir -p instead of install -d
    Former-commit-id: a7c928447c1ddf2b89de068aa175f2112cfa493f
    Former-commit-id: f3c12643d40de8f2ad767f2f5449132b4aeeefca

--- src/system-install.mk
+++ src/system-install.mk
@@ -54,8 +54,8 @@ install/nix/$(appsrcname)_x32.xpm: $(ICON)
 icons: $(ICONS)
 
 system-install-client: client
-	install -d $(libexecdir)/$(appname)
-	install -d $(gamesbindir)
+	$(MKDIR) $(libexecdir)/$(appname)
+	$(MKDIR) $(gamesbindir)
 	install -m755 $(appclient) $(libexecdir)/$(appname)/$(appname)
 	install -m755 install/nix/$(appsrcname).am \
 		$(gamesbindir)/$(appname)
@@ -70,9 +70,9 @@ system-install-client: client
 		$(libexecdir)/$(appname)/game
 
 system-install-server: server
-	install -d $(libexecdir)/$(appname)
-	install -d $(gamesbindir)
-	install -d $(datadir)/$(appname)
+	$(MKDIR) $(libexecdir)/$(appname)
+	$(MKDIR) $(gamesbindir)
+	$(MKDIR) $(datadir)/$(appname)
 	install -m755 $(appserver) \
 		$(libexecdir)/$(appname)/$(appname)-server
 	install -m755 install/nix/$(appsrcname)-server.am \
@@ -86,14 +86,13 @@ system-install-server: server
 		$(datadir)/$(appname)/version.cfg
 
 system-install-data:
-	install -d $(datadir)/$(appname)
-	install -d $(datadir)/$(appname)/game
+	$(MKDIR) $(datadir)/$(appname)/game
 	cp -r ../data $(datadir)/$(appname)/data
 	cp -r ../$(appgamedir) $(datadir)/$(appname)/game
 
 system-install-docs: $(MANPAGES)
-	install	-d $(mandir)/man6
-	install -d $(docdir)/$(appname)
+	$(MKDIR) $(mandir)/man6
+	$(MKDIR) $(docdir)/$(appname)
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
@@ -112,13 +111,13 @@ system-install-docs: $(MANPAGES)
 	cp ../doc/guidelines.txt $(docdir)/$(appname)/guidelines.txt
 
 system-install-menus: icons
-	install -d $(menudir)
-	install -d $(icondir)/16x16/apps
-	install -d $(icondir)/32x32/apps
-	install -d $(icondir)/48x48/apps
-	install -d $(icondir)/64x64/apps
-	install -d $(icondir)/128x128/apps
-	install -d $(pixmapdir)
+	$(MKDIR) $(menudir)
+	$(MKDIR) $(icondir)/16x16/apps
+	$(MKDIR) $(icondir)/32x32/apps
+	$(MKDIR) $(icondir)/48x48/apps
+	$(MKDIR) $(icondir)/64x64/apps
+	$(MKDIR) $(icondir)/128x128/apps
+	$(MKDIR) $(pixmapdir)
 	sed -e 's,@LIBEXECDIR@,$(patsubst $(DESTDIR)%,%,$(libexecdir)),g' \
 		-e 's,@DATADIR@,$(patsubst $(DESTDIR)%,%,$(datadir)),g' \
 		-e 's,@DOCDIR@,$(patsubst $(DESTDIR)%,%,$(docdir)),g' \
@@ -139,11 +138,11 @@ system-install-menus: icons
 		$(pixmapdir)/$(appname).xpm
 
 system-install-cube2font: cube2font system-install-cube2font-docs
-	install -d $(bindir)
+	$(MKDIR) $(bindir)
 	install -m755 cube2font $(bindir)/cube2font
 
 system-install-cube2font-docs: ../doc/man/cube2font.1
-	install -d $(mandir)/man1
+	$(MKDIR) $(mandir)/man1
 	gzip -9 -n -c < ../doc/man/cube2font.1 \
 		> $(mandir)/man1/cube2font.1.gz
 
