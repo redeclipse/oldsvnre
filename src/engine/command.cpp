6156abc19396b4ad2759b2099564f408fa32986e
Lee Salzman <lsalzman@gmail.com>
2013-07-25 06:52:51 +0000

    loop command stuff
    Former-commit-id: dd8e980eca226cf564068bbf8658f5cc5ce9f95b
    Former-commit-id: 110c5a5e17b76b7cf946c9d59ee6e48979abe6f9

--- src/engine/command.cpp
+++ src/engine/command.cpp
@@ -2410,6 +2410,40 @@ ICOMMAND(0, do, "e", (uint *body), executeret(body, *commandret));
 ICOMMAND(0, if, "tee", (tagval *cond, uint *t, uint *f), executeret(getbool(*cond) ? t : f, *commandret));
 ICOMMAND(0, ?, "ttt", (tagval *cond, tagval *t, tagval *f), result(*(getbool(*cond) ? t : f)));
 
+ICOMMAND(0, pushif, "rTe", (ident *id, tagval *v, uint *code),
+{
+    if(id->type != ID_ALIAS || id->index < MAXARGS) return;
+    if(getbool(*v))
+    {
+        identstack stack;
+        pusharg(*id, *v, stack);
+        v->type = VAL_NULL;
+        id->flags &= ~IDF_UNKNOWN;
+        executeret(code, *commandret);
+        poparg(*id);
+    }
+});
+
+void loopiter(ident *id, identstack &stack, tagval &v)
+{
+    if(id->stack != &stack)
+    {
+        pusharg(*id, v, stack);
+        id->flags &= ~IDF_UNKNOWN;
+    }
+    else
+    {
+        if(id->valtype == VAL_STR) delete[] id->val.s;
+        cleancode(*id);
+        id->setval(v);
+    }
+}
+
+void loopend(ident *id, identstack &stack)
+{
+    if(id->stack == &stack) poparg(*id);
+}
+
 static inline void setiter(ident &id, int i, identstack &stack)
 {
     if(i)
