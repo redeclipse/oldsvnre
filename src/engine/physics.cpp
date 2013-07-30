fcdcbb3815dfa71ee7366d8ba0cacb21a7579e91
Lee Salzman <lsalzman@gmail.com>
2013-07-30 16:54:48 +0000

    typo fix
    Former-commit-id: 1f9535c7d8e503880ddfe050c6546dc12340a017
    Former-commit-id: 62c9d34427dd3e1c7224506b0aeeb2f41f9eeed5

--- src/engine/physics.cpp
+++ src/engine/physics.cpp
@@ -1191,7 +1191,7 @@ bool collide(physent *d, const vec &dir, float cutoff, bool playercol)
     hitflags = HITFLAG_NONE;
     wall.x = wall.y = wall.z = 0;
     ivec bo(int(d->o.x-d->radius), int(d->o.y-d->radius), int(d->o.z-d->height)),
-         bs(int(d->o.x+d->radius), int(d->o.y+d->radius), int(d->o.z++d->aboveeye));
+         bs(int(d->o.x+d->radius), int(d->o.y+d->radius), int(d->o.z+d->aboveeye));
     bs.add(1);  // guard space for rounding errors
     if(!octacollide(d, dir, cutoff, bo, bs)) return false;//, worldroot, ivec(0, 0, 0), hdr.worldsize>>1)) return false; // collide with world
     return !playercol || plcollide(d, dir);
