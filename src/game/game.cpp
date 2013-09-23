205fde669acf61db3e96c536a50629a70ad8e1f6
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-09-23 06:07:06 +0000

    delay switching ability in waiting mode for the deathbuttonmash inclined
    Former-commit-id: 8215a2cb82dcadadc1d29401a4902bddc9542842
    Former-commit-id: 03391b7926687be5fcbfe356b5c9681eb00fc73e

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -209,6 +209,7 @@ namespace game
     VAR(IDF_PERSIST, deathanim, 0, 2, 2); // 0 = hide player when dead, 1 = old death animation, 2 = ragdolls
     VAR(IDF_PERSIST, deathfade, 0, 1, 1); // 0 = don't fade out dead players, 1 = fade them out
     VAR(IDF_PERSIST, deathscale, 0, 0, 1); // 0 = don't scale out dead players, 1 = scale them out
+    VAR(IDF_PERSIST, deathbuttonmash, 0, 1000, VAR_MAX);
     FVAR(IDF_PERSIST, bloodscale, 0, 1, 1000);
     VAR(IDF_PERSIST, bloodfade, 1, 3000, VAR_MAX);
     VAR(IDF_PERSIST, bloodsize, 1, 50, 1000);
@@ -640,7 +641,7 @@ namespace game
 
     bool followswitch(int n, bool other)
     {
-        if(player1->state == CS_SPECTATOR || player1->state == CS_WAITING)
+        if(player1->state == CS_SPECTATOR || (player1->state == CS_WAITING && (!player1->lastdeath || !deathbuttonmash || lastmillis-player1->lastdeath > deathbuttonmash)))
         {
             bool istv = tvmode(true, false);
             int *f = istv ? &spectvfollow : &follow;
