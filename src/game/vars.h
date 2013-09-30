681f2292fd42146c775e3cc2f6a21625b018a997
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-09-30 04:05:26 +0000

    restore saner resize limits
    Former-commit-id: c5233872d5a509737e1f5e89c5013f30c09c6553
    Former-commit-id: 9fc09aaf93e156435e1b907d67f2266697fd8302

--- src/game/vars.h
+++ src/game/vars.h
@@ -189,9 +189,9 @@ GFVAR(0, maxhealth, 0, 1.5f, FVAR_MAX);
 GFVAR(0, maxhealthvampire, 0, 3.0f, FVAR_MAX);
 
 GFVAR(0, actorscale, FVAR_NONZERO, 1, FVAR_MAX);
-GFVAR(0, maxresizescale, 1, 3.0f, FVAR_MAX);
-GFVAR(0, minresizescale, FVAR_NONZERO, 0.25f, 1);
-GFVAR(0, instaresizeamt, FVAR_NONZERO, 0.25f, 1); // each kill adds this much size in insta-resize
+GFVAR(0, maxresizescale, 1, 2, FVAR_MAX);
+GFVAR(0, minresizescale, FVAR_NONZERO, 0.5f, 1);
+GFVAR(0, instaresizeamt, FVAR_NONZERO, 0.1f, 1); // each kill adds this much size in insta-resize
 
 GVAR(0, burntime, 0, 5500, VAR_MAX);
 GVAR(0, burndelay, 0, 1000, VAR_MAX);
