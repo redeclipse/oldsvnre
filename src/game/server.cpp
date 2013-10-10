4ff3ce8f4f8d774ede72bf63b232feaaa9fae478
Quinton Reeves <qreeves@gmail.com>
2013-10-10 08:52:10 +0000

    operator fix
    Former-commit-id: 46e2458ed47ae85ddd13879496beca4188a9bdc6
    Former-commit-id: fe94cdcc2e86dbfa0c94730cf7f04e0c4448c896

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -2512,7 +2512,7 @@ namespace server
         loopv(clients) if(clients[i] && clients[i] != ci)
         {
             clientinfo *cp = clients[i];
-            if(cp->state.actortype != A_PLAYER || (!newteam || cp->team != newteam) || !cp->swapteam || cp->swapteam != oldteam) continue;
+            if(cp->state.actortype != A_PLAYER || (newteam && cp->team != newteam) || !cp->swapteam || cp->swapteam != oldteam) continue;
             cp->swapteam = T_NEUTRAL;
             setteam(cp, oldteam, (m_balreset(gamemode) ? TT_DEFAULT : 0)|TT_INFO, false);
             ancmsgft(cp->clientnum, S_V_BALALERT, CON_EVENT, "\fyyou have been moved to %s as previously requested", colourteam(oldteam));
