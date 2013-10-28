447071b9a1596c9bcaef170bd8338f31b2b9c159
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2013-10-28 09:14:36 +0000

    menu tweaks
    Former-commit-id: 990a7e8c6ac66b1ccaf9c5bbf55eba8dac834330
    Former-commit-id: 5fe67893766f7694d81e98313d04dc6e1f453b28

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1855,7 +1855,7 @@ namespace game
         resetcursor();
         if(!empty)
         {
-            conoutft(CON_MESG, "\fs%s\fS by \fs%s\fS \fs[\fa%s\fS]", *maptitle ? maptitle : "Untitled", *mapauthor ? mapauthor : "Unknown", server::gamename(gamemode, mutators));
+            conoutft(CON_SELF, "\fs%s\fS by \fs%s\fS, playing: \fs\fa%s\fS", *maptitle ? maptitle : "Untitled", *mapauthor ? mapauthor : "Unknown", server::gamename(gamemode, mutators));
             preload();
         }
         // reset perma-state
