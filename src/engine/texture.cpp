a77dedde40124d27082e70d2e4d172250f2b6fdd
eihrul <eihrul@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2014-01-13 18:32:13 +0000

    texturedata fix
    Former-commit-id: 26d02b041117ea80ac30862e7c6be0802d182b2b
    Former-commit-id: 5a58b5a6e53057183cc12c5415a96fc6edd38908

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -1201,7 +1201,7 @@ static bool texturedata(ImageData &d, const char *tname, Slot::Tex *tex = NULL, 
         else file++;
     }
 
-    if(!file) { if(msg) conoutf("\frcould not load texture: %s", tname); return NULL; }
+    if(!file) { if(msg) conoutf("\frcould not load texture: %s", tname); return false; }
 
     bool raw = !usedds || !compress, dds = false;
     for(const char *pcmds = cmds; pcmds;)
