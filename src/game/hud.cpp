0b80f33df6d9f3f5a003a8bc54860d72184273c1
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2014-02-04 03:55:28 +0000

    more UI tweaks
    Former-commit-id: 1d1d3a079aa45ce789ee8833e09f53b29487e25c
    Former-commit-id: d255a6894541c10ccae25241760f2dae30d56bbd

--- src/game/hud.cpp
+++ src/game/hud.cpp
@@ -47,7 +47,7 @@ namespace hud
     FVAR(IDF_PERSIST, commandfadeskew, 0, 0, 1);
     FVAR(IDF_PERSIST, commandscale, FVAR_NONZERO, 1, FVAR_MAX);
     VAR(IDF_PERSIST, uifade, 0, 250, VAR_MAX);
-    FVAR(IDF_PERSIST, uifadeamt, 0, 0.75f, 1);
+    FVAR(IDF_PERSIST, uifadeamt, 0, 0.5f, 1);
 
     int conskip = 0;
     void setconskip(int *n)
