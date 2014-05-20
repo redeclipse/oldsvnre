9d7c193ca97f64c8af49e7b225cf2abd522e5dd5
Lee Salzman <lsalzman@gmail.com>
2014-05-20 19:24:08 +0000

    zip tga fix
    Former-commit-id: e0714d1eacb0132084e5f282cbeed362d67f8628
    Former-commit-id: b38548656bf0fc3da1112c6c0a25b2be648c901f

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -3230,7 +3230,9 @@ SDL_Surface *loadsurface(const char *name, bool noload)
             SDL_RWops *rw = z->rwops();
             if(rw)
             {
-                s = IMG_Load_RW(rw, 0);
+                const char *ext = strrchr(name, '.');
+                if(ext) ++ext;
+                s = IMG_LoadTyped_RW(rw, 0, ext);
                 SDL_FreeRW(rw);
             }
             delete z;
