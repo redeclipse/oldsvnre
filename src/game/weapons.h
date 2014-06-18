fc4bfbab18960100c50d5abf81c3070fa2c7ada0
Quinton Reeves <qreeves@gmail.com>
2014-06-18 10:08:19 +0000

    allow negative fragrel
    Former-commit-id: 391bd1d8ebc38cb5a81e374c0b5ef4c01e73f6af
    Former-commit-id: 556012073c0837d03ce8baf7e30093d5109be2b9

--- src/game/weapons.h
+++ src/game/weapons.h
@@ -259,7 +259,7 @@ WPVARM(0, fragrays, 0, MAXPARAMS,
     5,          5,          5,          5,          5,          5,          5,          5,          25,         30,         35,
     5,          5,          5,          20,         30,         5,          5,          5,          25,         10,         35
 );
-WPFVARM(0, fragrel, 0, FVAR_MAX,
+WPFVARM(0, fragrel, FVAR_MIN, FVAR_MAX,
     1.0f,       1.0f,       1.0f,       1.0f,       1.0f,       1.0f,       1.0f,       1.0f,       0.0f,       0.0f,       0.0f,
     1.0f,       1.0f,       1.0f,       1.5f,       0.1f,       1.0f,       1.0f,       1.0f,       0.0f,       0.0f,       0.0f
 );
