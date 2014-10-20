de70116197064e24db6ae53bf9e1722de06ce11b
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2014-10-20 08:09:10 +0000

    fix namespace
    Former-commit-id: 3fe703d568a42bdd6948d91a2cbb8e6e5fe39b63
    Former-commit-id: 7cd8c5c6180a5cbf5f091f5e3fa0452fc250289f

--- src/game/bomber.cpp
+++ src/game/bomber.cpp
@@ -544,7 +544,7 @@ namespace bomber
         if(f.owner)
         {
             if(!d->ai || f.owner != d) return;
-            bool forever = m_ffa(gamemode, mutators) || d->health >= m_health(game::gamemode, game::mutators, d->model)/3 || findtarget(d) < 0;
+            bool forever = m_ffa(game::gamemode, game::mutators) || d->health >= m_health(game::gamemode, game::mutators, d->model)/3 || findtarget(d) < 0;
             if(!carrytime && forever) return;
             int takemillis = lastmillis-f.taketime, length = forever ? carrytime-550-bomberlockondelay : min(carrytime, 1000);
             if(takemillis >= length)
