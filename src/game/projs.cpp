fc05e3d65ad69e790dacc7192901d7adc3c34a2e
qreeves <qreeves@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2014-10-29 11:41:07 +0000

    tweak projtrails to be a little less intensive
    Former-commit-id: a43cec156ff36634bda777c1194d1806b8a1bd45
    Former-commit-id: cdeceddd400588db408921cbdefdc630f24ca412

--- src/game/projs.cpp
+++ src/game/projs.cpp
@@ -46,8 +46,8 @@ namespace projs
     FVAR(IDF_PERSIST, ejectweight, -10000, 180, 10000);
 
     VAR(IDF_PERSIST, projtrails, 0, 1, 1);
-    VAR(IDF_PERSIST, projtraildelay, 2, 10, VAR_MAX);
-    VAR(IDF_PERSIST, projtraillength, 1, 350, VAR_MAX);
+    VAR(IDF_PERSIST, projtraildelay, 2, 15, VAR_MAX);
+    VAR(IDF_PERSIST, projtraillength, 1, 250, VAR_MAX);
     VAR(IDF_PERSIST, projhints, 0, 1, 6);
     VAR(IDF_PERSIST, projfirehint, 0, 1, 1);
     FVAR(IDF_PERSIST, projhintblend, 0, 0.75f, 1);
