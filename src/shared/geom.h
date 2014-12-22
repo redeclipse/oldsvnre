ec8b7a4f640c857bddf88af3bcfd13784c912f79
eihrul <eihrul@2cd6abe5-5779-42d9-9038-cec04ae5ff40>
2014-12-22 20:15:06 +0000

    more bvec fixes
    Former-commit-id: 3df81e482c86f925f9bf4fa2c67838835951238f
    Former-commit-id: 9aee796b67d1d5c12f32683088dfc95da7bfbc47

--- src/shared/geom.h
+++ src/shared/geom.h
@@ -1111,7 +1111,7 @@ struct bvec
 
     bvec() {}
     bvec(uchar x, uchar y, uchar z) : x(x), y(y), z(z) {}
-    bvec(const vec &v) : x(uchar((v.x+1)*255/2)), y(uchar((v.y+1)*255/2)), z(uchar((v.z+1)*255/2)) {}
+    bvec(const vec &v) : x(uchar((v.x+1)*(255.0f/2.0f))), y(uchar((v.y+1)*(255.0f/2.0f))), z(uchar((v.z+1)*(255.0f/2.0f))) {}
     explicit bvec(int color) : x((color>>16)&0xFF), y((color>>8)&0xFF), z(color&0xFF) {}
     explicit bvec(const bvec4 &v);
 
